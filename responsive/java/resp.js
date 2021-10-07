df122e70b33942d6a2afcc69dc71be946544839f
huzidev <huzaifa.iqdev@gmail.com>
2021-10-08 03:49:24 +0500

    park

---
+++ responsive/java/resp.js
@@ -0,0 +1,12 @@
+burger = document.querySelector('.burger')
+navBar = document.querySelector('.nav-bar')
+navList = document.querySelector('.nav-list')
+rightNav = document.querySelector('.right-nav')
+
+
+
+burger.addEventListener('click', ()=>{
+    right-nav.classList.toggle('v-resp');
+    nav-list.classList.toggle('v-resp');
+    nav-bar.classList.toggle('v-resp');
+})
