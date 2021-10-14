fb0907f37a04bfafb99af77e6aeef536da6989cd
huzidev <huzaifa.iqdev@gmail.com>
2021-10-14 20:48:53 +0500

    Responsive

---
+++ first work/java.js
@@ -0,0 +1,12 @@
+burger = document.querySelector('.burger')
+navBar = document.querySelector('#nav-bar')
+items = document.querySelector('.items')
+item = document.querySelector('.item')
+
+
+
+burger.addEventListener('click', ()=>{
+    item.classList.toggle('item');
+    items.classList.toggle('items');
+    navBar.classList.toggle('nav-bar');
+})
