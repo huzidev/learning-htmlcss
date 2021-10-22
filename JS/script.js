8b6a81497972ffc22c6b7dee7d52f82f1850d41d
huzidev <huzaifa.iqdev@gmail.com>
2021-10-22 19:37:42 +0500

    first name by funtionm

--- JS/script.js
+++ JS/script.js
@@ -3,14 +3,14 @@ console.log(
 )
 
 function greet(names){
-    console.log(names + "Is A Good Boy")
+    console.log(names + "Is A Good Boy");
 }
-
 let names = "Huzi"
 let name1 = "Logan"
 let name2 = "Sahil"
 let name3 = "Ali"
 let name4= "Haasan"   
+greet(names);
 // This Can Be A Method But You Can Also Use One More Efficient And Fast Method
 // console.log(names + " is A Good Boy")
 // console.log(name2 + " is A Good Boy")
