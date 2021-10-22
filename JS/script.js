0380cfb7cda89a833f7007848cc8ec5ce178643f
huzidev <huzaifa.iqdev@gmail.com>
2021-10-22 20:06:41 +0500

    by default greet

--- JS/script.js
+++ JS/script.js
@@ -2,21 +2,21 @@ console.log(
     "Hello hi ok"
 )
 // you can add multiple types of varaibales just put greet parameterss
-function greet(names, greetText){
+function greet(names, greetText="Hello From JS"){
     console.log(greetText);
     console.log(names + " Is A Good Boy");
 }
-let names = "Huzi"
-let name1 = "Logan"
-let name2 = "Sahil"
-let name3 = "Ali"
-let name4= "Haasan"   
-let greetText = "Good Morning Bro You Know What,"
+let names = "Huzi";
+let name1 = "Logan";
+let name2 = "Sahil";
+let name3 = "Ali";
+let name4= "Haasan";   
+let greetText = "Good Morning Bro You Know What,";
 greet(names, greetText);
 greet(name1, greetText);
 greet(name2, greetText);
 greet(name3, greetText);
-greet(name4, greetText);
+greet(name4, );
 // This Can Be A Method But You Can Also Use One More Efficient And Fast Method
 // console.log(names + " is A Good Boy")
 // console.log(name2 + " is A Good Boy")
