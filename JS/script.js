fd6c49be0080f3b2e3f980870a9de01a8d15fadc
huzidev <huzaifa.iqdev@gmail.com>
2021-10-22 19:36:52 +0500

    names by function

--- JS/script.js
+++ JS/script.js
@@ -2,13 +2,17 @@ console.log(
     "Hello hi ok"
 )
 
+function greet(names){
+    console.log(names + "Is A Good Boy")
+}
+
 let names = "Huzi"
 let name1 = "Logan"
 let name2 = "Sahil"
 let name3 = "Ali"
 let name4= "Haasan"   
 // This Can Be A Method But You Can Also Use One More Efficient And Fast Method
-console.log(names + " is A Good Boy")
-console.log(name2 + " is A Good Boy")
-console.log(name3 + " is A Good Boy")
-console.log(name4 + " is A Good Boy")
+// console.log(names + " is A Good Boy")
+// console.log(name2 + " is A Good Boy")
+// console.log(name3 + " is A Good Boy")
+// console.log(name4 + " is A Good Boy")
