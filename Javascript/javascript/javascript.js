da46e384b2439272fab7ea0a139af0ce5cbc0865
huzidev <huzaifa.iqdev@gmail.com>
2021-10-25 13:39:06 +0500

    commented for JS

--- Javascript/javascript/javascript.js
+++ Javascript/javascript/javascript.js
@@ -4,3 +4,5 @@ console.log("Hello, Learing JS with Jade/Sass");
 // DOM with JS
 let mainContainer = document.getElementById("main-container"); 
 console.log(mainContainer);
+
+// MAKE SURE TO PUT SEMI-COLON AT VERY END OBVIOUS WHILE USING JADE/SASS
