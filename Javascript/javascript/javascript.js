3c337c00164fee98cc837a46b7df9ed88f58854b
huzidev <huzaifa.iqdev@gmail.com>
2021-10-25 13:46:29 +0500

    added element by class

--- Javascript/javascript/javascript.js
+++ Javascript/javascript/javascript.js
@@ -2,8 +2,13 @@ console.log("Hello, Learing JS with Jade/Sass");
 
 
     // DOM with JS
+
 let mainContainer = document.getElementById("main-container"); 
 console.log(mainContainer);
+
     // WHILE USING VARIABLE LIKE (VAR/LET) MAKE SURE TO USE JUST ONE WORD AND NOT TO USE DESH (-) AFTER VARIABLES BCZ AN ERROR COULD OCCUR WHILE (" INSIDE SEMI-COLON USE AS YOU WANTED")
 
     // MAKE SURE TO PUT SEMI-COLON AT VERY END OBVIOUS WHILE USING JADE/SASS other wise an error would appear
+
+let nav = document.getElementsByClassName("nav");
+console.log(nav);
