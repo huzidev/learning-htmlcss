54b68bd4acf4f087e4c78a40c16681e46f6cebb5
huzidev <huzaifa.iqdev@gmail.com>
2021-10-25 18:49:26 +0500

    toggle fix

--- Javascript/javascript/javascript.js
+++ Javascript/javascript/javascript.js
@@ -40,6 +40,7 @@ ParentChild1[4].innerHTML = "new button"
 function toggleHide(){
     let btn = document.getElementById("btn");   
     let para = document.getElementById("para");
+    
     if(para.style.display != "none"){
     para.style.display = "none";
     }
