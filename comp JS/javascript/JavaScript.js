68239167cd6fb86ce4053e7c92d947934a669de6
HuziDev <huzaifa.iqdev@gmail.com>
2021-11-16 19:04:11 +0500

    time bar fixed

--- comp JS/javascript/JavaScript.js
+++ comp JS/javascript/JavaScript.js
@@ -3,16 +3,14 @@ console.log("learing JS")
 
 // Adding Live Time By JS
 
+// Date & Time
 
-// function displayTime() {
+    let day = new Date("2021-11-16")
+    document.getElementById("day").innerHTML = day;    
+
+//     function displayTime() {
 //     let time = new Date();
 //     console.log(time);  
 //     document.getElementById("time").innerHTML = time;
 // }
 // setInterval(displayTime, 1000);
-
-
-// Date & Time
-
-    let day = new Date("2021-11-16")
-    document.getElementById("day").innerHTML = day;    
