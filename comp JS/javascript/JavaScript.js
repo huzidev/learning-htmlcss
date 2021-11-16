a51c71ee17e28c36402768fa82d62bfc6562f876
HuziDev <huzaifa.iqdev@gmail.com>
2021-11-16 19:08:33 +0500

    styling by JS

--- comp JS/javascript/JavaScript.js
+++ comp JS/javascript/JavaScript.js
@@ -8,9 +8,11 @@ console.log("learing JS")
     let day = new Date("2021-11-16")
     document.getElementById("day").innerHTML = day;    
 
-//     function displayTime() {
-//     let time = new Date();
-//     console.log(time);  
-//     document.getElementById("time").innerHTML = time;
-// }
-// setInterval(displayTime, 1000);
+    // function displayTime() {
+    // let time = new Date();
+    // console.log(time);  
+    // document.getElementById("time").innerHTML = time;}
+    // setInterval(displayTime, 1000);
+
+
+    document.getElementById
