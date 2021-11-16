b3aa9a1dd4cd885a2c53698e5fb7bd9093d775f8
HuziDev <huzaifa.iqdev@gmail.com>
2021-11-16 18:41:08 +0500

    time added

--- comp JS/javascript/JavaScript.js
+++ comp JS/javascript/JavaScript.js
@@ -1,1 +1,12 @@
 console.log("learing JS")
+
+
+// Adding Live Time By JS
+
+
+function displayTime() {
+    time = new Date();
+    console.log(time);  
+    document.getElementById("time").innerHTML = time;
+}
+setInterval(displayTime, 2000);
