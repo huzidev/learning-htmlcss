f55959cb24f6ead1435347025a2145222b1e5640
HuziDev <huzaifa.iqdev@gmail.com>
2021-11-16 19:02:02 +0500

    added date

--- comp JS/javascript/JavaScript.js
+++ comp JS/javascript/JavaScript.js
@@ -4,9 +4,15 @@ console.log("learing JS")
 // Adding Live Time By JS
 
 
-function displayTime() {
-    time = new Date();
-    console.log(time);  
-    document.getElementById("time").innerHTML = time;
-}
-setInterval(displayTime, 1000);
+// function displayTime() {
+//     let time = new Date();
+//     console.log(time);  
+//     document.getElementById("time").innerHTML = time;
+// }
+// setInterval(displayTime, 1000);
+
+
+// Date & Time
+
+    let day = new Date("2021-11-16")
+    document.getElementById("day").innerHTML = day;    
