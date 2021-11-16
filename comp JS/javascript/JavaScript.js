57069bfaa837d121c5944a968ef8628bc3b96f06
HuziDev <huzaifa.iqdev@gmail.com>
2021-11-16 22:48:36 +0500

    operands js

--- comp JS/javascript/JavaScript.js
+++ comp JS/javascript/JavaScript.js
@@ -65,8 +65,9 @@ console.log("learing JS")
      `;
 
 let a = 5;
-console.log(a);
+console.log(a); 
 let b = 5;
+b = -b;
 console.log(b);
 console.log("The Sum oF A and B is equal to", +  (a + b));
 
