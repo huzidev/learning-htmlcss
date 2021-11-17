76bb0ed400ba3b9de45186e0714e8e9991e5c427
HuziDev <huzaifa.iqdev@gmail.com>
2021-11-17 17:27:56 +0500

    if works

--- comp JS/javascript/JavaScript.js
+++ comp JS/javascript/JavaScript.js
@@ -183,5 +183,8 @@ console.log(z);
 // IF ,ELSE OR ELSE IF
 let age = 32;
 if (age>18){
-    console.log("you can go to the party");
+    alert("you can go to the party");
+}
+else{
+    alert("you cant");
 }
