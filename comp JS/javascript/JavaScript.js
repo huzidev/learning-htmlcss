e177df2d9da31fd3691e652eece3a9ded1e7303d
HuziDev <huzaifa.iqdev@gmail.com>
2021-11-19 21:20:08 +0500

    fix style bar

--- comp JS/javascript/JavaScript.js
+++ comp JS/javascript/JavaScript.js
@@ -620,9 +620,9 @@ document.getElementById("js-nav").innerHTML = `
             </p>
             <img src="/assets/linux.png" id="linux" alt="Linux-Image">
             
-            // WHEN WE ADD THE IMAGE IT OVERFLOW THE PARENT CONATINER STARTS OVERFLOWING FROM CONTAINER SO WE JUST HAVE TO ADD (OVERFLOW = AUTO) IN THE PARENT CONTAINER TO FIX IT
     </div>
 `
+            // WHEN WE ADD THE IMAGE IT OVERFLOW THE PARENT CONATINER STARTS OVERFLOWING FROM CONTAINER SO WE JUST HAVE TO ADD (OVERFLOW = AUTO) IN THE PARENT CONTAINER TO FIX IT
 
 
 function toggleHide() {
