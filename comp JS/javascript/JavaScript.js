fa53a967cc8fe45fb8b6b30436d784403a9ddd1f
HuziDev <huzaifa.iqdev@gmail.com>
2021-11-19 10:37:25 +0500

    round up and round down

--- comp JS/javascript/JavaScript.js
+++ comp JS/javascript/JavaScript.js
@@ -501,4 +501,8 @@ console.log(`3 raised to the power of 3 is ${Math.pow (3, 3)} is and 3 raised to
 
 // SQUARE ROOT 
 
-console.log(`Square root of 64 is ${Math.sqrt(64)}`)
+console.log(`Square root of 64 is ${Math.sqrt(64)}`;
+
+
+// ROUND UP AND DOUND DOWN
+console)
