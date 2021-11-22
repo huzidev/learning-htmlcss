d964a978a5da30acd73462f8e2a70347d42a7f0c
HuziDev <huzaifa.iqdev@gmail.com>
2021-11-22 18:54:49 +0500

    comment

--- comp JS/javascript/JavaScript.js
+++ comp JS/javascript/JavaScript.js
@@ -824,4 +824,4 @@ console.log(location.search); // ON WHICH PAGE WE WERE ON LIKE (? PAGE = ONE)
 
 // document.write(location.href); //WILL PRINT THE LINK ON OURS WEBPAGE
 
-location.href = "https://www.google.com";
+location.href = "https://www.google.com"; //WILL ALWAYS LEAD YOU TO THE LINK I REPEAT ALWAYS
