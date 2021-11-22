bfcb6c9549a3cc0d63a82d1d39200c7174cae295
HuziDev <huzaifa.iqdev@gmail.com>
2021-11-22 18:48:48 +0500

    location HASH

--- comp JS/javascript/JavaScript.js
+++ comp JS/javascript/JavaScript.js
@@ -819,3 +819,4 @@ console.log(location.hostname);
 console.log(location.href); //WILL GAVE THE URL OF WEBPAGE
 console.log(location.port);
 console.log(location.protocol);//WILL TELL THAT ON WHCIH PROTOCOL WEB IS MADE OF LIKE (HTTP)
+console.log(location.hash); //WILL PROVIDE INFO ABOUT ON WHICH PAGE WE WERE ON LIKE (#HOME), (#ABOUT)
