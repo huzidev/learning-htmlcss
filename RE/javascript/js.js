64ba533070aa8e2b531ddda9a0301923c53174c6
HuziDev <huzaifa.iqdev@gmail.com>
2021-11-23 11:11:25 +0500

    array functions

--- RE/javascript/js.js
+++ RE/javascript/js.js
@@ -11,3 +11,5 @@ console.log("The Sum Of a & b is " + (a + b));
 let name = "Huzi";
 let hobby = "Learning";
 let code = "JavsScript";
+let advise = "Keep The Good Spirit"
+console.log(`The name of the developer is ${name} and he's coding in ${code} and his hobby is ${hobby}`)
