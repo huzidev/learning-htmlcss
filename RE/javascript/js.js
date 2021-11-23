caf557d8d5bc1fcb3e2abf7103af99f4dc7e6da3
HuziDev <huzaifa.iqdev@gmail.com>
2021-11-23 14:13:35 +0500

    new string

--- RE/javascript/js.js
+++ RE/javascript/js.js
@@ -27,7 +27,7 @@ console.log(`The length of developer's name is ${name.length} and his hobby whic
 
 console.log(`Hello Dev-${name} \n How r u`);
 
-let str = new String (`${name}`);
+let str = new String (`Mr ${name} yours ${code} coding is Great ${advise}`);
 console.log(str);
 
-console.log(`Overall the length of ours new-string is `)
+console.log(`Overall the length of ours new-string is ` + str.length)
