7383b9d67d151d1b2b9d7090be444cf6a906e6bf
HuziDev <huzaifa.iqdev@gmail.com>
2021-11-24 10:35:00 +0500

    indendtation

--- RE/javascript/js.js
+++ RE/javascript/js.js
@@ -285,11 +285,13 @@ console.log(data);
 console.log("ARRAY FUNCTION");
 let array = [name= "Huzi", Hobby= "Learning",  Code="JavaScript", Age= "19"];
 console.log(array);
-
 console.log(`The length of whole (ARRAY) FUNCTION is ` + array.length);
 
 
 console.log("ARRAY MESSAGE");
+
 let dev = [`The name of the developer is ${name} and he love ${hobby} and he codes in ${code} and he should've to continue the work and ${advise}`];
+
 console.log(dev);
+
 console.log(`The length of array message is just ` + (dev.length) + ` because it is just a single mesaage in a single array function`)
