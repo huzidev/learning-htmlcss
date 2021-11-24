01015b96ef88fdd85b6d2d1c1e03d6f966c425fd
HuziDev <huzaifa.iqdev@gmail.com>
2021-11-24 10:34:00 +0500

    array function

--- RE/javascript/js.js
+++ RE/javascript/js.js
@@ -290,3 +290,4 @@ console.log(`The length of whole (ARRAY) FUNCTION is ` + array.length);
 
 let dev = [`The name of the developer is ${name} and he love ${hobby} and he codes in ${code} and he should've to continue the work and ${advise}`];
 console.log(dev);
+console.log(`The length of array message is just ` + (dev.length) + ` because it is just a single mesaage in a single array function`)
