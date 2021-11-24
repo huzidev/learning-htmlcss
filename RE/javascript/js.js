403b92beec8c7d9a8ae9d51075a07f6e19c161c6
HuziDev <huzaifa.iqdev@gmail.com>
2021-11-24 20:31:42 +0500

    substrating function

--- RE/javascript/js.js
+++ RE/javascript/js.js
@@ -401,3 +401,5 @@ let total = (a, b) => {
 total = (a, b) => a + b;
 console.log(total(a, b));
 console.log("The above result is accurate because a is equal to " + a + " and the value of b is " + b);
+
+total = (a, b)
