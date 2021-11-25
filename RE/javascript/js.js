8051e73ac7e53ddc949990a473ddf89b7a255114
HuziDev <huzaifa.iqdev@gmail.com>
2021-11-25 12:40:16 +0500

    absoulte funcction complete

--- RE/javascript/js.js
+++ RE/javascript/js.js
@@ -501,4 +501,4 @@ console.log(info.toUpperCase());
  console.log(`The value is ${r} now the round off to the nearest integer will be ${Math.ceil(r)} and ${r} will round down to the ${Math.floor(r)} which is the nearest integers`);
 
  console.log(`\n`);
- console.log(`Value specifiv value is ${b} which is -ve`);
+ console.log(`The specifi value is ${b} which is -ve now after we put absolute function it'll become +ve ${Math.abs(b)};`);
