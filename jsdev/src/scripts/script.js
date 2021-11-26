33a93a99da265f62d4cf75b6c4aed59db249624e
HuziDev <huzaifa.iqdev@gmail.com>
2021-11-26 18:00:22 +0500

    comment

--- jsdev/src/scripts/script.js
+++ jsdev/src/scripts/script.js
@@ -7,15 +7,15 @@ window.onload = function () {
 
     setInterval(() => {
         loopSlides();
-    }, 4000);
+    }, 2000);
 }
 
 function loopSlides() {
     const slides = document.getElementsByClassName('slide');
     slides[activeIndex].classList.remove('active');
-    activeIndex++;
+    activeIndex++; // ENCRYMENT
     if (activeIndex == slides.length) {
-        activeIndex = 0;
+        activeIndex = 0; // MEANS WHEN SLIDES LENGTH BECOME EQUALS TO SLIDES.LENGTH IT'LL BECOME EQUAL TO 0 (WHICH IS THE FIRST CHILD) MEANS IT'LL REPEATS IT SELF AGAIN
     }
     slides[activeIndex].classList.add('active');
 
