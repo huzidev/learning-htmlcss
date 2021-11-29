a45072177af74d3b1d918e7442f3ac092e13299f
HuziDev <huzaifa.iqdev@gmail.com>
2021-11-29 16:21:05 +0500

    loop function

--- practice slider/script/script.js
+++ practice slider/script/script.js
@@ -1,8 +1,12 @@
 let index = 1
-    window.onload = function () {
-        const slide = document.getElementsByClassName("slide");
-        slide[index].classList.add("active");
-    setInterval(() => {
-        loop()
+window.onload = function () {
+    const slide = document.getElementsByClassName("slide");
+    slide[index].classList.add("active");
+setInterval(() => {
+    loop()
     }, 2000);
 };
+
+function loop() {
+    console slide = document.getElementsByClassName("slide");    
+}
