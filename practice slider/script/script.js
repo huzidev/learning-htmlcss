82e7ffe18d225fcbe7cc4e7e8dbeb4bf09f10632
HuziDev <huzaifa.iqdev@gmail.com>
2021-11-29 17:29:43 +0500

    slide complete

--- practice slider/script/script.js
+++ practice slider/script/script.js
@@ -1,5 +1,18 @@
 let index = 1;
 setInterval(function(){
-    document.getElementById(`${radio} + ${index}`).checked = true;
-    counter++; // INCREMENT
-}, 2000);
+    document.getElementById("radio" + index).checked = true;
+    index++; // INCREMENT 
+    if (index > 4 ) {
+        index = 1;
+    }
+}, 3000);
+
+
+// var counter = 1;
+// setInterval(function(){
+//     document.getElementById("radio" + counter).checked = true;
+//     counter++;
+//     if (counter > 4) {
+//         counter = 1
+//     }
+// }, 2000);
