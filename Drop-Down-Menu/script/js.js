7b27ad712e53c4105c5c7e590d586c967c4fb312
huzidev <huzaifa.iqdev@gmail.com>
2021-12-03 18:42:15 +0500

    check\

---
+++ Drop-Down-Menu/script/js.js
@@ -0,0 +1,10 @@
+console.log("Live Time By JS");
+
+let date = new Date("2021-12-03");
+date = document.getElementById("date").innerHTML = date;
+console.log(date);
+
+let day = new Date();
+day = document.getElementById("day").innerHTML = day;
+
+console.log(day);
