a897ad2868506719c27aba1de5b7e9aa57392b6f
huzidev <huzaifa.iqdev@gmail.com>
2021-12-06 15:50:25 +0500

    loops added

--- drop-down/script/js.js
+++ drop-down/script/js.js
@@ -3,6 +3,8 @@ let index = 0;
 window.onload = function() {
     let slides = document.getElementsByClassName("slide");
     slides[slide].classList.add("active");
-    
+    setInterval(() => {
+        loops();
+    }, 2000);
 }
 
