599336d8757e5a8e92a4898a0aeb142977e724e1
huzidev <huzaifa.iqdev@gmail.com>
2021-12-06 15:58:58 +0500

    increment

--- drop-down/script/js.js
+++ drop-down/script/js.js
@@ -11,8 +11,10 @@ window.onload = function() {
 function loop() {
     const slides = document.getElementsByClassName("slide");
     slides[index].classList.add("active");
-    slides[index].classList.remove("active")
+    slides[index].classList.remove("active");
+    index++;
     if (index == slides.length) {
         index = 0;  
-    }
+    };
+    slides[index].classList.add("active")
 }
