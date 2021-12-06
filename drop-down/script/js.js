1f34b3bc4621ffd6730eca6f439e7c37e974bbec
huzidev <huzaifa.iqdev@gmail.com>
2021-12-06 15:53:39 +0500

    index to start

--- drop-down/script/js.js
+++ drop-down/script/js.js
@@ -12,6 +12,6 @@ function loop() {
     const slides = document.getElementsByClassName("slide");
     slides[index].classList.add("active");
     if (index == slides.length) {
-        
+        index = 0;
     }
 }
