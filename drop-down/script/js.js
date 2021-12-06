7a6ebaefe3dd3f9e72dca315363970d50cbc8cab
huzidev <huzaifa.iqdev@gmail.com>
2021-12-06 15:51:35 +0500

    constant function

--- drop-down/script/js.js
+++ drop-down/script/js.js
@@ -1,8 +1,8 @@
 let index = 0;
 
 window.onload = function() {
-    let slides = document.getElementsByClassName("slide");
-    slides[slide].classList.add("active");
+    const slides = document.getElementsByClassName("slide");
+    slides[index].classList.add("active");
     setInterval(() => {
         loops();
     }, 2000);
