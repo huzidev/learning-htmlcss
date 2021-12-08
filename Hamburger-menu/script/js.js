d45fbcaeadeef893053d4266d1da8d7ba8ccd69f
huzidev <huzaifa.iqdev@gmail.com>
2021-12-08 12:55:09 +0500

    class-list add

--- Hamburger-menu/script/js.js
+++ Hamburger-menu/script/js.js
@@ -2,5 +2,7 @@ const menuBtn = document.querySelector(".menu-btn");
 let menuOpen = false;
 
 menuBtn.addEventListener("click", () =>{
-
+    if (!menuBtn) {
+        menuBtn.classList.add("open")
+    }
 })
