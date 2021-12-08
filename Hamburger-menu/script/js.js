e0dff03aa4e5c3caf4e3fd3680fe2411d3960725
huzidev <huzaifa.iqdev@gmail.com>
2021-12-08 12:56:04 +0500

    indentation

--- Hamburger-menu/script/js.js
+++ Hamburger-menu/script/js.js
@@ -2,7 +2,8 @@ const menuBtn = document.querySelector(".menu-btn");
 let menuOpen = false;
 
 menuBtn.addEventListener("click", () =>{
-    if (!menuBtn) {
-        menuBtn.classList.add("open")
-    }
-})
+    if (!menuOpen) {
+        menuBtn.classList.add("open");
+        menuOpen = true;
+    };
+});
