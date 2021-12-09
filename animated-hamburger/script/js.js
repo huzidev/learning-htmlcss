7b15149a30210a829cd59da7eb8c3c4be74a7e3f
huzidev <huzaifa.iqdev@gmail.com>
2021-12-09 14:49:04 +0500

    nav-bar toglle

--- animated-hamburger/script/js.js
+++ animated-hamburger/script/js.js
@@ -3,6 +3,5 @@ const navBar = document.querySelector(".nav-bar");
 
 
 menuIcon.addEventListener("click", () =>{
-    navBar.classList.toggle("change");
-    // WE DONT HAVE TO USE CLASS ADD OR CLASS REMOVE IT WONT WORK HERE INSTEAD WE JUST USED TOGGLE
+    navBar.classList.toggle("change")
 });
