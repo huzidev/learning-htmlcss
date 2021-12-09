fef8f21b4b59925d3696f2407ada347fe0930762
huzidev <huzaifa.iqdev@gmail.com>
2021-12-09 14:41:52 +0500

    toggle function

--- animated-hamburger/script/js.js
+++ animated-hamburger/script/js.js
@@ -1,2 +1,8 @@
 const menuIcon = document.querySelector(".hamburger-menu");
 const navBar = document.querySelector(".nav-bar");
+
+
+menuIcon.addEventListener("click", () =>{
+    navBar.classList.toggle("change")
+
+})
