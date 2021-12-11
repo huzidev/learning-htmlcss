5b650b512ac53750b8c3638bab81a953d9ad4697
huzidev <huzaifa.iqdev@gmail.com>
2021-12-11 10:08:55 +0500

    toggle event

--- dev-javscript-webpage/script/js.js
+++ dev-javscript-webpage/script/js.js
@@ -2,5 +2,5 @@ const menuIcon = document.querySelector(".hamburger");
 const navBar = document.querySelector(".nav-bar");
 
 menuIcon.addEventListener("click", () => {
-
+    navBar.classList.toggle("change");
 })
