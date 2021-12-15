501b0562bdef98433cad4227a784df6a82215053
huzidev <huzaifa.iqdev@gmail.com>
2021-12-15 18:43:33 +0500

    comment

---
+++ dev-javscript-webpage/script/ball.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    const box = document.getElementsByClassName('ball')[0]; // THIS ZERO MEANS STARTING FROM ZERO LIKE WE USED IN SLIDER (INDEX = 0)
+    window.addEventListener('scroll', function(e) {
+        const height = document.body.clientHeight - window.innerHeight;
+        const width = document.body.clientWidth;
+        const scroll = window.scrollY;
+        
+        const ratio = (scroll / height);
+        const dy = ratio * window.innerHeight;
+        const dx = ratio * width;
+        console.log({scroll, height, width});
+        console.log({dy, dx, ratio});
+        box.style.top = `${dy}px`;
+        box.style.left = `${dx}px`;
+        // console.log(height, offset, box.clientHeight);
+    });
+}
