d746e76f4f87752285d5c3738c273e12c24891a4
huzidev <huzaifa.iqdev@gmail.com>
2021-12-15 20:40:48 +0500

    height for scroll

--- dev-javscript-webpage/script/square.js
+++ dev-javscript-webpage/script/square.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     const square = document.getElementsByClassName("square")[0]; // THIS ZERO MEANS STARTING FROM ZERO LIKE WE USED IN SLIDER (INDEX = 0)
     window.addEventListener("scroll", function () {
-        
+        const height = 
     })
 };
