bbe5498dd269ee6a52b71fef6e95de20d0e03708
huzidev <huzaifa.iqdev@gmail.com>
2021-12-15 20:41:32 +0500

    inner height

--- dev-javscript-webpage/script/square.js
+++ dev-javscript-webpage/script/square.js
@@ -1,6 +1,7 @@
 window.onload = function () {
     const square = document.getElementsByClassName("square")[0]; // THIS ZERO MEANS STARTING FROM ZERO LIKE WE USED IN SLIDER (INDEX = 0)
     window.addEventListener("scroll", function () {
-        const height = 
+        const height = document.body.clientHeight - window.innerHeight;
+        cons
     })
 };
