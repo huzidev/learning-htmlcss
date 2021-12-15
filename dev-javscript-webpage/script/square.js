602f81e4c56b1456811e9c65861c674f2cf1450d
huzidev <huzaifa.iqdev@gmail.com>
2021-12-15 20:37:42 +0500

    event listener

--- dev-javscript-webpage/script/square.js
+++ dev-javscript-webpage/script/square.js
@@ -1,3 +1,4 @@
 window.onload = function () {
-    const square = document.getElementsByClassName("square")[0]; // THIS ZERO MEANS STARTING FROM ZERO LIKE WE USED IN SLIDER (INDEX = 0)    
+    const square = document.getElementsByClassName("square")[0]; // THIS ZERO MEANS STARTING FROM ZERO LIKE WE USED IN SLIDER (INDEX = 0)
+    window.addEventListener
 };
