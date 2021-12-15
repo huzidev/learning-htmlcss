c4cae8dffa2dacd986e39a941f6eed8f868320bf
huzidev <huzaifa.iqdev@gmail.com>
2021-12-15 20:37:15 +0500

    comment

--- dev-javscript-webpage/script/square.js
+++ dev-javscript-webpage/script/square.js
@@ -1,3 +1,3 @@
 window.onload = function () {
-    const square = document.getElementsByClassName("square")    
-}
+    const square = document.getElementsByClassName("square")[0]; // THIS ZERO MEANS STARTING FROM ZERO LIKE WE USED IN SLIDER (INDEX = 0)    
+};
