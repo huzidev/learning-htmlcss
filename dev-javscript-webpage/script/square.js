d16ef5a10c3f94979525f94e8b9de2ac272be424
huzidev <huzaifa.iqdev@gmail.com>
2021-12-15 20:42:20 +0500

    scroll y

--- dev-javscript-webpage/script/square.js
+++ dev-javscript-webpage/script/square.js
@@ -2,6 +2,6 @@ window.onload = function () {
     const square = document.getElementsByClassName("square")[0]; // THIS ZERO MEANS STARTING FROM ZERO LIKE WE USED IN SLIDER (INDEX = 0)
     window.addEventListener("scroll", function () {
         const height = document.body.clientHeight - window.innerHeight;
-        const width = 
+        const width = document.body.clientWidth;// WE DIDN'T USED (- INNERWIDTH ) BECAUSE WE HAVEN'T ANY HORIZONTAL SCROLL 
     })
 };
