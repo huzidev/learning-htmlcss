ff3d3afd65e4cc048099014117f4cbd6ef04f875
huzidev <huzaifa.iqdev@gmail.com>
2021-12-15 20:53:13 +0500

    window innerheight

--- dev-javscript-webpage/script/square.js
+++ dev-javscript-webpage/script/square.js
@@ -5,7 +5,7 @@ window.onload = function () {
         const width = document.body.clientWidth;// WE DIDN'T USED (- INNERWIDTH ) BECAUSE WE HAVEN'T ANY HORIZONTAL SCROLL 
         const scroll = window.scrollY // BECAUSE WE WANTED ONLY VERTICAL SCROLL THEREFORE (SCROLLY)
         const ratio = (scroll / height); // SCROLL / HEIGHT BECAUSE WE JUST WANTED RESULT OF HEIGHT ACCORDING TO THE RESPECTIVE SCROLL
-        const dy = 
+        const dy = ratio * window.innerHeight
 
     });
 };
