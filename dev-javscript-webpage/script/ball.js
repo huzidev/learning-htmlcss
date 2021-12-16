6584261bdd2a570c608e5989aeb683a61b5050cb
huzidev <huzaifa.iqdev@gmail.com>
2021-12-16 18:27:02 +0500

    comment

--- dev-javscript-webpage/script/ball.js
+++ dev-javscript-webpage/script/ball.js
@@ -7,7 +7,7 @@ window.addEventListener("load", function(e) {  // SOME TIMES WHAT ACTUALLY HAPPE
         const ratio = (scroll / height); // SCROLL / HEIGHT BECAUSE WE JUST WANTED RESULT OF HEIGHT ACCORDING TO THE RESPECTIVE SCROLL
         const dy = ratio * window.innerHeight; // RATIO WHICH IS EQUAL TO (SCROLL/HEIGHT ) AS WE MENTIONED AND WE MULTIPLY BY INNERHEIGHT BECAUSE WE USED CLIENTHEIGHT - WINDOW.INNERHEIGHT
         const dx = ratio * width;
-        console.log({scroll, height, width});
+        console.log({ scroll, height, width }, document.body.clientHeight, window.innerHeight);
         console.log({dy, dx, ratio});
         box.style.top = `${dy}px`; // SO WE CAN STYLE OURS WEBSITE JUST LIKE CSS BY USING (.STYLE.) AS WE DID THERE SINCE (BOX) IS THE NAME OF OURS CLASS WE USED ABOVE THEREFORE WE USES (BOX.STYLE.) WE CAN ALSO GAVE IT SOME MARGIN, PADDING OR ANY OTHER PROPERTY JUST BY USING THIS FUNCTION
         box.style.left = `${dx}px`;
