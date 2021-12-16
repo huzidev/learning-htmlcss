7a52c2984c37340415f87f128c3f9901498595d0
huzidev <huzaifa.iqdev@gmail.com>
2021-12-16 20:37:10 +0500

    tittle

--- Math-Dimensions/script/js.js
+++ Math-Dimensions/script/js.js
@@ -2,7 +2,7 @@ window.onload = function () {
   const ball = document.getElementsByClassName("ball")[0];
   window.addEventListener("scroll", function () {
         const height = document.body.clientHeight - window.innerHeight;
-        const width = this.document.body.clientWidth;
+        const width = document.body.clientWidth;
         const scroll = window.scrollY;
         const ratio = ( scroll / height );
         const dy = ratio * window.innerHeight;
