2ab78b525c5856c650b146b1acdeef2b00e83ff2
huzidev <huzaifa.iqdev@gmail.com>
2021-12-16 18:44:15 +0500

    client width

--- Dimensions/script/js.js
+++ Dimensions/script/js.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   const ball = document.getElementsByClassName("ball")[0];
   window.addEventListener("scroll", function () {
-      
+        const height = document.body.clientHeight - window.innerHeight;
+        const width
   });
 };
