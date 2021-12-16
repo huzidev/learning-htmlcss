2b4046512a6351c4ea37ffae021176ac0a7b581e
huzidev <huzaifa.iqdev@gmail.com>
2021-12-16 19:01:40 +0500

    client height fix

---
+++ Math-Dimensions/script/js.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+  const ball = document.getElementsByClassName("ball")[0];
+  window.addEventListener("scroll", function () {
+        const height = document.body.clientHeight - window.innerHeight;
+        const width = this.document.body.clientWidth;
+        const scroll = window.scrollY;
+        const ratio = ( scroll / height );
+        const dy = ratio * window.innerHeight;
+        const dx = ratio * width;
+        ball.style.top = `${dy}px`;
+        ball.style.left = `${dx}px`;
+    console.log({ height, width, ratio, dx, dy, scroll, }, document.body.clientHeight, window.innerHeight);
+  });
+};
