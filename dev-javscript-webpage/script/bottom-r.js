5846658b33fe38808d51ceb0696ae4e204460e82
huzidev <huzaifa.iqdev@gmail.com>
2021-12-16 16:21:46 +0500

    client wdith

--- dev-javscript-webpage/script/bottom-r.js
+++ dev-javscript-webpage/script/bottom-r.js
@@ -1,6 +1,7 @@
 window.addEventListener("laod", function (BR) {
     const bottomR = document.getElementsByClassName("bottom-right")[0];
     window.addEventListener("scroll", function (BR) {
-       const height = document.body.clientHeight - window.innerHeight; 
+       const height = document.body.clientHeight - window.innerHeight;
+       const width = document.body.clientWidth;
     });
 });
