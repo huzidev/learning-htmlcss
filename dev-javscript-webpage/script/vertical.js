3fa7e02cd2cf9288d97d80c030367d9658067c67
huzidev <huzaifa.iqdev@gmail.com>
2021-12-16 12:32:58 +0500

    ratio for web-page

--- dev-javscript-webpage/script/vertical.js
+++ dev-javscript-webpage/script/vertical.js
@@ -13,4 +13,5 @@ window.addEventListener("load", function (v2) {
     const vertical2 = document.getElementsByClassName("vertical-2")[0];
     const height = document.body.clientHeight - window.innerHeight;
     const scroll = window.scrollY;
+    const ratio = (scroll / height); 
 });
