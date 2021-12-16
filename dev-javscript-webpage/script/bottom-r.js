9ee2f59d1f5df78bb4f33d28b2a44c51a950b423
huzidev <huzaifa.iqdev@gmail.com>
2021-12-16 15:18:31 +0500

    client width

--- dev-javscript-webpage/script/bottom-r.js
+++ dev-javscript-webpage/script/bottom-r.js
@@ -2,5 +2,6 @@ window.addEventListener("load", function (BR) {
    const bottomR = document.getElementsByClassName("bottom-left");
    window.addEventListener("scroll", function (BR) {
        const height = document.body.clientHeight - window.innerHeight;
+       const width = document.body.clientWidth;
    }) 
 });
