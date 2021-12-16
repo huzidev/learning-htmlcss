54937e185a7a4026cd19370c2d9cce3f32bfdaac
huzidev <huzaifa.iqdev@gmail.com>
2021-12-16 11:40:25 +0500

    scroll function

--- dev-javscript-webpage/script/vertical.js
+++ dev-javscript-webpage/script/vertical.js
@@ -1,4 +1,6 @@
 window.addEventListener("load", function (v) {
     const vertical1 = document.getElementsByClassName("vertical-1")[0];
-    
+    window.addEventListener("scroll", function (v) {
+        
+    })
 })
