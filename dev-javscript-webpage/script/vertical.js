54461ed51375a5b962b0195ba5bc530aa61f4c5f
huzidev <huzaifa.iqdev@gmail.com>
2021-12-16 12:36:52 +0500

    scroll event

--- dev-javscript-webpage/script/vertical.js
+++ dev-javscript-webpage/script/vertical.js
@@ -11,9 +11,11 @@ window.addEventListener("load", function (v1) {
 
 window.addEventListener("load", function (v2) {
     const vertical2 = document.getElementsByClassName("vertical-2")[0];
+    this.window
     const height = document.body.clientHeight - window.innerHeight;
     const scroll = window.scrollY;
     const ratio = ( scroll / height );
     const dy = ratio * window.innerHeight;
-    vertical2.style.top = `${dy}px`; 
+    vertical2.style.top = `${dy}px`;
+    vertical2.style.marginRight = `100px` 
 }, false);
