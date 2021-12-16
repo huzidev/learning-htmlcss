36a32a3fd298b66ea4f59b1caca9c85730f822ef
huzidev <huzaifa.iqdev@gmail.com>
2021-12-16 15:20:05 +0500

    indentations

--- dev-javscript-webpage/script/bottom-r.js
+++ dev-javscript-webpage/script/bottom-r.js
@@ -4,6 +4,7 @@ window.addEventListener("load", function (BR) {
        const height = document.body.clientHeight - window.innerHeight;
        const width = document.body.clientWidth;
        const scroll = window.scrollY;
-       const ratio ( scroll / height);
+       const ratio = ( scroll / height);
+
    }) 
 });
