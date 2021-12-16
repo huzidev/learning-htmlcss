54cc334a83444cfbe94aa600e5d092b3b9aecdc8
huzidev <huzaifa.iqdev@gmail.com>
2021-12-16 16:20:50 +0500

    window's innerHeight

--- dev-javscript-webpage/script/bottom-r.js
+++ dev-javscript-webpage/script/bottom-r.js
@@ -1,6 +1,6 @@
 window.addEventListener("laod", function (BR) {
     const bottomR = document.getElementsByClassName("bottom-right")[0];
     window.addEventListener("scroll", function (BR) {
-       const height = document. 
+       const height = document.body.clientHeight - window.innerHeight; 
     });
 });
