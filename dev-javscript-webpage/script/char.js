cfeaa681762640784f6af39f8fd3f2087fd6ceb7
huzidev <huzaifa.iqdev@gmail.com>
2021-12-17 10:29:29 +0500

    event listener

--- dev-javscript-webpage/script/char.js
+++ dev-javscript-webpage/script/char.js
@@ -3,4 +3,7 @@ let scrollfix;
 function character() {
     let char = document.getElementById("character-base");
     let des = document.getElementById("character-desc");
+    window.addEventListener("scroll", function () {
+        
+    });
 };
