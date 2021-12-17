9e118a26790481844af9189ff31b4784ee46fe4e
huzidev <huzaifa.iqdev@gmail.com>
2021-12-17 10:41:13 +0500

    conditons for vertical scroll

--- dev-javscript-webpage/script/char.js
+++ dev-javscript-webpage/script/char.js
@@ -9,6 +9,7 @@ function character() {
            let cond = {hello: window.innerHeight - 180};
            cond.want = cond.hello + document.getElementById("about").offsetHeight - 100;
            cond.best = cond.want + document.getElementById("status").offsetHeight + 240;
+           cond.contact = cond.best + document.getElementById("portfolio").offsetHeight + 750;
        }, 100); 
     });
 };
