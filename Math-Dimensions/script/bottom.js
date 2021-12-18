a7a931ad9c79d509f1f27c58ae681f70130dcc56
huzidev <huzaifa.iqdev@gmail.com>
2021-12-18 18:28:43 +0500

    client width

--- Math-Dimensions/script/bottom.js
+++ Math-Dimensions/script/bottom.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", function () {
     let ball1 = document.getElementById("ball-1")[0];
     window.addEventListener("scroll", function () {
-        
+        const height = document.body.clientHeight - window.innerHeight;
+        const width = document.body.clientWidth;
     });
 });
