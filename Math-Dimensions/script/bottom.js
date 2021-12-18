9e63008d766151f4df0e200c98cd94bcb9b4d681
huzidev <huzaifa.iqdev@gmail.com>
2021-12-18 18:56:38 +0500

    results

--- Math-Dimensions/script/bottom.js
+++ Math-Dimensions/script/bottom.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function () {
     const square = document.getElementsByClassName("ball-1")[0];
     const SQ = {h: square.clientHeight, w: square.clientWidth};
-    const result = { y: (wind.innerHeight - SQ.h), }
+    const result = { y: (wind.innerHeight - SQ.h), w: (clientWidth - SQ.w)};
     window.addEventListener("scroll", function () {
         const height = document.body.clientHeight - window.innerHeight;
         const width = document.body.clientWidth;
