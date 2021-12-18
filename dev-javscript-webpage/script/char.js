db70b282f62da1a3c555fb975910e386eb622c13
huzidev <huzaifa.iqdev@gmail.com>
2021-12-18 09:54:50 +0500

    comment

--- dev-javscript-webpage/script/char.js
+++ dev-javscript-webpage/script/char.js
@@ -7,8 +7,8 @@ function character() {
        clearTimeout(scrollfix);
        scrollfix = setTimeout(() => {
            let cond = {hello: window.innerHeight - 180};
-           cond.looking = cond.hello + document.getElementById("about").offsetHeight - 100;
-           cond.skills = cond.looking + document.getElementById("status").offsetHeight + 240;
+           cond.looking = cond.hello + document.getElementById("about").offsetHeight - 100; // OFFSET HEIGHT INCLUDES VIEWABLE HEIGHT INCLUDES PADDING, BORDER & SCROLL-BAR BUT NOT THE MARGIN
+           cond.skills = cond.looking + document.getElementById("status").offsetHeight + 240;  
            cond.contact = cond.skills + document.getElementById("portfolio").offsetHeight + 750;
        }, 100); 
     });
