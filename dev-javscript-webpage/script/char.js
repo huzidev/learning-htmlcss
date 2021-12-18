160c0b58d7bfdd33a6f9da11c531c71d5a59a1ab
huzidev <huzaifa.iqdev@gmail.com>
2021-12-18 10:22:15 +0500

    descriptions

--- dev-javscript-webpage/script/char.js
+++ dev-javscript-webpage/script/char.js
@@ -22,6 +22,7 @@ function character() {
             }
             if (this.scrollY > cond.looking && !scroll.looking) {
                 scroll.looking = true;
+                desc.innerHTML = "Are You Looking For A Developer?"
             }
 
        }, 100); 
