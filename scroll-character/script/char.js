c1bc0d2b7852ea6e276f2cfb28ab5f9be8902717
huzidev <huzaifa.iqdev@gmail.com>
2021-12-18 17:10:20 +0500

    character fix

--- scroll-character/script/char.js
+++ scroll-character/script/char.js
@@ -8,9 +8,9 @@ function character() {
         scrollFix = setTimeout(() => {
             let cond = {hello: window.innerHeight - 180};
             cond.hope = cond.hello + document.getElementById("about").offsetHeight - 100;
-            cond.looking = cond.hope + document.getElementById("highlight").offsetHeight + 200;
-            cond.skills = cond.looking + document.getElementById("skills").offsetHeight + 400;
-            cond.contact = cond.skills + document.getElementById("work").offsetHeight + 700;
+            cond.looking = cond.hope + document.getElementById("highlight").offsetHeight - 100;
+            cond.skills = cond.looking + document.getElementById("skills").offsetHeight - 100;
+            cond.contact = cond.skills + document.getElementById("work").offsetHeight + 150;
             if (this.scrollY > cond.hello && !scroll.hello) {
                 scroll.hello = true;
                 desc.innerHTML = "Hello, How Are You?";
@@ -52,6 +52,35 @@ function character() {
             }
             if (this.scrollY > cond.skills && !scroll.skills) {
                 scroll.skills = true;
+                desc.innerHTML = "Just Have! A Glance That How Good This Guy Is"
+                desc.classList.add("skills");
+                char.classList.add("skills");
+            }
+            else if (this.scrollY < cond.skills && scroll.skills) {
+                scroll.skills = false;
+                desc.innerHTML = "Hope! You Were Fine"
+                desc.classList.remove("skills");
+                char.classList.remove("skills");
+                desc.classList.add("hope");
+                char.classList.add("hope");
+            }
+            if (this.scrollY > cond.contact && !scroll.contact) {
+                scroll.contact = true;
+                desc.innerHTML = "Just Contact Him :)"; 
+            }
+            if (this.scrollY < cond.contact && scroll.contact) {
+                scroll.contact = false;
+                desc.innerHTML = "Just Have! A Glance That How Good This Guy Is";
+            }
+            if (this.scrollY < cond.hello) {
+                desc.classList.remove("hello");
+                char.classList.remove("hello");
+                desc.classList.remove("hope");
+                char.classList.remove("hope");
+                desc.classList.remove("looking");
+                char.classList.remove("looking");
+                desc.classList.remove("skills");
+                char.classList.remove("skills");
             }
         }, 100);
     });
