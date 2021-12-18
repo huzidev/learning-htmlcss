bf3631fd0b665fc0dcfc6736510c8a7384961fcc
huzidev <huzaifa.iqdev@gmail.com>
2021-12-18 15:27:08 +0500

    set-timeout

--- scroll-character/script/char.js
+++ scroll-character/script/char.js
@@ -1,9 +1,12 @@
-let scroll;
+let scrollFix;
 
 function character() {
     let desc = document.getElementById("character-desc");
     let char = document.getElementById("character-char");
     window.addEventListener("scroll", function () {
-        
+        clearTimeout(scrollFix);
+        scrollFix = setTimeout(() => {
+            
+        }, 100);
     });
 };
