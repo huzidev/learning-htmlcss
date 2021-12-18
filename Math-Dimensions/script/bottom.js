68c1e721318226f7631aca54609e4f84e66150b3
huzidev <huzaifa.iqdev@gmail.com>
2021-12-18 18:33:03 +0500

    scroll behaviour

--- Math-Dimensions/script/bottom.js
+++ Math-Dimensions/script/bottom.js
@@ -4,6 +4,10 @@ window.addEventListener("load", function () {
         const height = document.body.clientHeight - window.innerHeight;
         const width = document.body.clientWidth;
         const scroll = window.scrollY;
-        const ratio = scroll / height;
+        const ratio = (scroll / height);
+        const dy = ratio * clientHeight;
+        const dx = ratio * clientWidth;
+        ball1.style.bottom = `${dy}px`;
+        ball1.style.left = `${dx}px`;
     });
 });
