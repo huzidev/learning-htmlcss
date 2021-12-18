a342449ae1d2f5496a8937e8d59dac0f542bf3ce
huzidev <huzaifa.iqdev@gmail.com>
2021-12-18 19:11:55 +0500

    merge

--- Math-Dimensions/script/bottom.js
+++ Math-Dimensions/script/bottom.js
@@ -1,15 +1,16 @@
 window.addEventListener("load", function () {
     const square = document.getElementsByClassName("ball-1")[0];
     const SQ = {h: square.clientHeight, w: square.clientWidth};
-    const result = { y: (window.innerHeight - SQ.h), x: (window.clientWidth - SQ.w)};
-    ball.style.bottom = result.y+`px`;
-    ball.style.left = result.x+`px`;
+    const result = { y: (window.innerHeight - SQ.h), x: (window.innerWidth - SQ.w)};
+    square.style.bottom = result.y+'px';
+    square.style.left = result.x+'px';
     window.addEventListener("scroll", function () {
         const height = document.body.clientHeight - window.innerHeight;
         const width = document.body.clientWidth;
         const scroll = window.scrollY;
         const ratio = (scroll / height);
         const dy = result.y - (ratio * window.innerHeight);
+        const dx = result.x - (ratio * window.innerWidth);
         square.style.bottom = `${dy}px`;
         square.style.left = `${dx}px`;
     });
