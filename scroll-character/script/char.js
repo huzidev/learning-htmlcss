5d136f5695895815d18d9ecfca598b6a3fb8b96d
huzidev <huzaifa.iqdev@gmail.com>
2021-12-18 15:25:39 +0500

    event listener

--- scroll-character/script/char.js
+++ scroll-character/script/char.js
@@ -3,4 +3,7 @@ let scroll;
 function character() {
     let desc = document.getElementById("character-desc");
     let char = document.getElementById("character-char");
+    window.addEventListener("scroll", function () {
+        
+    })
 }
