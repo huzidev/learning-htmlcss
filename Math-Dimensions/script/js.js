96375138d39b1d216632139a3d89c9de0fb7cef0
huzidev <huzaifa.iqdev@gmail.com>
2021-12-18 18:22:25 +0500

    merge

--- Math-Dimensions/script/js.js
+++ Math-Dimensions/script/js.js
@@ -1,15 +1,18 @@
 window.onload = function () {
   const ball = document.getElementsByClassName("ball")[0];
+  const bd = {h : ball.clientHeight, w: ball.clientWidth};
+  const init = { y: (window.innerHeight - bd.h), x: (window.innerWidth - bd.w) };
+  ball.style.top = init.y+'px';
+  ball.style.left = init.x+'px';
   window.addEventListener("scroll", function () {
         const height = document.body.clientHeight - window.innerHeight;
         const width = document.body.clientWidth;
         const scroll = window.scrollY;
-        const ratio = ( scroll / height );
-        const yratio = ( window.innerHeight * ratio) ;
-        const dy = window.innerHeight - yratio;
-        const dx = ratio * width;
+        const ratio = scroll / height;
+        const dy = init.y - (ratio * window.innerHeight);
+        const dx = init.x - (ratio * window.innerWidth);
         ball.style.top = `${dy}px`
-        ball.style.right = `${dx}px`;
+        ball.style.left = `${dx}px`;
     console.log({ height, width, ratio, dx, dy, scroll, }, document.body.clientHeight, window.innerHeight, yratio);
   });
 };
