b825610b682fce54fa8edec7759664f47b310c7f
huzidev <huzaifa.iqdev@gmail.com>
2021-12-18 16:45:41 +0500

    characters

--- scroll-character/script/char.js
+++ scroll-character/script/char.js
@@ -8,13 +8,21 @@ function character() {
         scrollFix = setTimeout(() => {
             let cond = {hello: window.innerHeight - 180};
             cond.hope = cond.hello + document.getElementById("about").offsetHeight - 100;
-            cond.looking = cond.hope + document.getElementById("highlight").offsetHeight - 200;
-            cond.skills = cond.looking + document.getElementById("skills").offsetHeight - 200;
+            cond.looking = cond.hope + document.getElementById("highlight").offsetHeight + 200;
+            cond.skills = cond.looking + document.getElementById("skills").offsetHeight + 400;
             cond.contact = cond.skills + document.getElementById("work").offsetHeight + 700;
             if (this.scrollY > cond.hello && !scroll.hello) {
                 scroll.hello = true;
                 desc.innerHTML = "Hello, How Are You?";
+                desc.classList.add("hello");
+                char.classList.add("hello");
+            }
+            else if (this.scrollY < cond.hello && scroll.hello) {
+                scroll.hello = false
+                desc.classList.remove("hello");
+                char.classList.remove("hello");
             }
         }, 100);
     });
 };
+character();
