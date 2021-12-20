10dfd685f1f98509b03708eb9c82b4e5683211a5
huzidev <huzaifa.iqdev@gmail.com>
2021-12-20 10:13:09 +0500

    dimensions

--- Math-Dimensions/script/bottom.js
+++ Math-Dimensions/script/bottom.js
@@ -2,8 +2,8 @@ window.addEventListener("load", function () {
     const square = document.getElementsByClassName("ball-1")[0];
     const SQ = {h: square.clientHeight, w: square.clientWidth};
     const result = { y: (window.innerHeight - SQ.h), x: (window.innerWidth - SQ.w)};
-    square.style.bottom = result.y+'px';
-    square.style.left = result.x+'px';
+    square.style.top = result.y+'px'; // MEANS IT'LL TOOK IT FROM TOP TO BOTTOM
+    square.style.right = result.x+'px'; // MEANS IT'LL TOOK IT FROM RIGHT TO LEFT
     window.addEventListener("scroll", function () {
         const height = document.body.clientHeight - window.innerHeight;
         const width = document.body.clientWidth;
