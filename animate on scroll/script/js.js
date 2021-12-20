ec00a071a3b47f56f671a15a242321b80b6fdc15
huzidev <huzaifa.iqdev@gmail.com>
2021-12-20 17:23:11 +0500

    for each

--- animate on scroll/script/js.js
+++ animate on scroll/script/js.js
@@ -2,4 +2,8 @@ window.addEventListener("scroll", reveal);
 
 function reveal() {
     const reveal = document.querySelectorAll(".reveal");
+    for (const i = 0; i < array.length; i++) {
+        const windowheight = window.innerHeight;
+        const element = array[i];
+    }
 }
