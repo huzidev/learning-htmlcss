c3d6d717f70ad7a932514cfe2770ff98bdedac12
huzidev <huzaifa.iqdev@gmail.com>
2021-12-20 14:59:29 +0500

    client height & width

--- Math-Dimensions/script/ball-2.js
+++ Math-Dimensions/script/ball-2.js
@@ -1,4 +1,4 @@
 window.addEventListener("load", function () {
    const ballR  = this.document.getElementsByClassName("ball-3")[0];
-   
+   const BR = {h: ballR.clientHeight, w: ballR.clientWidth};
 });
