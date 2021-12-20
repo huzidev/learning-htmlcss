9a4d7504735da1d50404cfb9817694d404d20066
huzidev <huzaifa.iqdev@gmail.com>
2021-12-20 15:01:50 +0500

    styling by JS

--- Math-Dimensions/script/ball-2.js
+++ Math-Dimensions/script/ball-2.js
@@ -2,4 +2,6 @@ window.addEventListener("load", function () {
    const ballR  = this.document.getElementsByClassName("ball-3")[0];
    const BR = {h: ballR.clientHeight, w: ballR.clientWidth};
    const res = {y: (window.innerHeight - BR.h), x: (window.innerWidth - BR.w)};
+   ballR.style.top = 
+   ballR.style.right = 
 });
