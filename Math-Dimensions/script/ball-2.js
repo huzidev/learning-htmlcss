9f0cb601ee2fecce02ceaa038e14393340499e69
huzidev <huzaifa.iqdev@gmail.com>
2021-12-20 15:00:11 +0500

    inner height

--- Math-Dimensions/script/ball-2.js
+++ Math-Dimensions/script/ball-2.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", function () {
    const ballR  = this.document.getElementsByClassName("ball-3")[0];
    const BR = {h: ballR.clientHeight, w: ballR.clientWidth};
+   const res = {y: (window.innerHeight - BR.h)}
 });
