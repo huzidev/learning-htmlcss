bf1b3fe42c4494a969f18a7efb6e9bd46831326a
huzidev <huzaifa.iqdev@gmail.com>
2021-12-20 12:50:01 +0500

    scroll functions

--- Math-Dimensions/script/ball-1.js
+++ Math-Dimensions/script/ball-1.js
@@ -3,5 +3,8 @@ window.addEventListener("load", function () {
    const R = { h: round.clientHeight, w: round.clientWidth};
    const final = { y: (window.innerHeight - R.h), x: (window.innerWidth - R.w)};
    round.style.bottom = final.y+"px";
-   round.style.left = final.x+"px"; 
+   round.style.left = final.x+"px";
+   window.addEventListener("scroll", function name(params) {
+       
+   })
 });
