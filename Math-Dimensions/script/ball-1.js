4b90e526cac6133ee2a6a5e57133a87f48df7612
huzidev <huzaifa.iqdev@gmail.com>
2021-12-20 12:50:59 +0500

    cliet height

--- Math-Dimensions/script/ball-1.js
+++ Math-Dimensions/script/ball-1.js
@@ -4,7 +4,7 @@ window.addEventListener("load", function () {
    const final = { y: (window.innerHeight - R.h), x: (window.innerWidth - R.w)};
    round.style.bottom = final.y+"px";
    round.style.left = final.x+"px";
-   window.addEventListener("scroll", function name(params) {
-       
+   window.addEventListener("scroll", function () {
+        const height = document.body.clientHeight - window.innerHeight;
    })
 });
