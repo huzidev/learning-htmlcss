c7e524fa488d319167ae3beae5db9a0e11487fb9
huzidev <huzaifa.iqdev@gmail.com>
2021-12-20 12:44:52 +0500

    formula added

--- Math-Dimensions/script/ball-1.js
+++ Math-Dimensions/script/ball-1.js
@@ -1,5 +1,7 @@
 window.addEventListener("load", function () {
    const round = document.getElementsByClassName("ball-2");
    const R = { h: round.clientHeight, w: round.clientWidth};
-   const final = { y: (innerHeight - R.h), x: (innerWidth - R.w)}; 
+   const final = { y: (innerHeight - R.h), x: (innerWidth - R.w)};
+   round.style.bottom = final.y+"px";
+   round.style.left = final.x+"px"; 
 });
