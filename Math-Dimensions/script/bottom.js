787957e38940dd4599dfeacfce3c52bd8cc239b1
huzidev <huzaifa.iqdev@gmail.com>
2021-12-20 10:16:06 +0500

    commetn

--- Math-Dimensions/script/bottom.js
+++ Math-Dimensions/script/bottom.js
@@ -1,7 +1,8 @@
 window.addEventListener("load", function () {
     const square = document.getElementsByClassName("ball-1")[0];
-    const SQ = {h: square.clientHeight, w: square.clientWidth};
+    const SQ = {h: square.clientHeight, w: square.clientWidth}; // MEANS PLACEMENT OF OURS SQUARE ACCORDING TO BODY'S CONTENT OR CLIENT HEIGHT AND WIDTH
     const result = { y: (window.innerHeight - SQ.h), x: (window.innerWidth - SQ.w)};
+    // SINCE WE PLACED OURS SQUARE ACCORDING TO BODY'S CONTENT NOW WE'LL SET IT'S POSITION ACCORDING TO INNERHEIGHT OR ACCORDING TO (VISIBLE) CONTENT 
     square.style.top = result.y+'px'; // MEANS IT'LL TOOK IT FROM TOP TO BOTTOM
     square.style.right = result.x+'px'; // MEANS IT'LL TOOK IT FROM RIGHT TO LEFT
     window.addEventListener("scroll", function () {
