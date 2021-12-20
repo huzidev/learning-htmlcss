c3b164b4877352a9e7fa653b98feb0d455145ea4
huzidev <huzaifa.iqdev@gmail.com>
2021-12-20 12:42:11 +0500

    fornula

--- Math-Dimensions/script/ball-1.js
+++ Math-Dimensions/script/ball-1.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function () {
    const round = document.getElementsByClassName("ball-2");
    const R = { h: round.clientHeight, w: round.clientWidth};
-   const final = {} 
+   const final = { y: (innerHeight - R.h), x: (innerWidth - R.w)}; 
 });
