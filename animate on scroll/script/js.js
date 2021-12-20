70d3213e80d79e77aadf3f36ce90812c97e97211
huzidev <huzaifa.iqdev@gmail.com>
2021-12-20 17:25:17 +0500

    if else

--- animate on scroll/script/js.js
+++ animate on scroll/script/js.js
@@ -4,6 +4,14 @@ function reveal() {
     const reveal = document.querySelectorAll(".reveal");
     for (const i = 0; i < array.length; i++) {
         const windowheight = window.innerHeight;
-        const element = array[i];
+        const revealtop = reveal[i].getBoundingClientRect().top;
+        const revealpoint = 150;
+        if (revealtop < windowheight - revealpoint) {
+            reveal[i].classList.add("active");
+        }
+        else{
+            reveal[i].classList.remove("active");
+        }
+
     }
 }
