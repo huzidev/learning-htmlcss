c9375c8ac08f7bc925d71c4fd9c57426c6803b5a
huzidev <huzaifa.iqdev@gmail.com>
2021-12-20 12:48:40 +0500

    scroll behaviour

--- Math-Dimensions/script/ball-1.js
+++ Math-Dimensions/script/ball-1.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function () {
-   const round = document.getElementsByClassName("ball-2");
+   const round = document.getElementsByClassName("ball-2")[0];
    const R = { h: round.clientHeight, w: round.clientWidth};
-   const final = { y: (innerHeight - R.h), x: (innerWidth - R.w)};
+   const final = { y: (window.innerHeight - R.h), x: (window.innerWidth - R.w)};
    round.style.bottom = final.y+"px";
    round.style.left = final.x+"px"; 
 });
