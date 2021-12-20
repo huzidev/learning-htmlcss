15440f90e62f1b8b89a84c85d6a36eb8bea87d9a
huzidev <huzaifa.iqdev@gmail.com>
2021-12-20 17:34:51 +0500

    reveal point

--- animate on scroll/script/js.js
+++ animate on scroll/script/js.js
@@ -5,7 +5,7 @@ function reveal() {
     for (const i = 0; i < reveal.length; i++) {
         const windowheight = window.innerHeight;
         const revealtop = reveal[i].getBoundingClientRect().top;
-        const revealpoint = 150;
+        const revealpoint = 150; //MEANS IF WE SCROLL 150PX IT'LL SCROLL THEN
         if (revealtop < windowheight - revealpoint) {
             reveal[i].classList.add("active");
         }
