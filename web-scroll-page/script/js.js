7fb9b41bdb2372b744e9c0cb0350413a10551717
huzidev <huzaifa.iqdev@gmail.com>
2021-12-21 16:40:11 +0500

    btn fixed

--- web-scroll-page/script/js.js
+++ web-scroll-page/script/js.js
@@ -1,30 +1,30 @@
 window.addEventListener("scroll", ()=>{
     let revealR = document.querySelectorAll(".revealR");
     for (let i = 0; i < revealR.length; i++) {
-    let content = revealR[i].getBoundingClientRect().top;
-    let screen = window.innerHeight;
-    if (content < screen) {
-        revealR[i].classList.add("active");
-    }
-    else {
-        revealR[i].classList.remove("active");
-    }
-};
+        let content = revealR[i].getBoundingClientRect().top;
+        let screen = window.innerHeight;
+        if (content < screen) {
+            revealR[i].classList.add("active");
+        }
+        else {
+            revealR[i].classList.remove("active");
+        }
+    };
 
 });
 
 window.addEventListener("scroll", ()=>{
     let revealB = document.querySelectorAll(".revealB");
     for (let i = 0; i < revealB.length; i++) {
-    let content = revealB[i].getBoundingClientRect().top;
-    let screen = window.innerHeight;
-    if (content < screen) {
-        revealB[i].classList.add("active")
-    } 
-    else{
-        revealB[i].classList.remove("active")
-    }
-};
+        let content = revealB[i].getBoundingClientRect().top;
+        let screen = window.innerHeight;
+        if (content < screen) {
+            revealB[i].classList.add("active")
+        } 
+        else{
+            revealB[i].classList.remove("active")
+        }
+    };
 });
 
 window.addEventListener("scroll", function () {
@@ -38,7 +38,7 @@ window.addEventListener("scroll", function () {
         else {
             revealT[i].classList.remove("active");
         }
-}
+    };
 });
 
 window.addEventListener("scroll", function () {
@@ -64,7 +64,7 @@ window.addEventListener("scroll", function () {
         else {
             revealL[i].classList.remove("active");
         }
-    }
+    };
 });
 
 
