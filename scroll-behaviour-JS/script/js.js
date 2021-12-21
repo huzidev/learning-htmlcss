71a886cd57a2522ec865532eb3f2e0d85bc29c74
huzidev <huzaifa.iqdev@gmail.com>
2021-12-21 18:17:09 +0500

    fade function

--- scroll-behaviour-JS/script/js.js
+++ scroll-behaviour-JS/script/js.js
@@ -1,3 +1,17 @@
+// window.addEventListener("scroll", function () {
+//     const reveal = this.document.querySelectorAll(".revealL");
+//     const content = reveal[i].getBoundingClientRect().top;
+// })
+
+
 window.addEventListener("scroll", function () {
     const reveal = this.document.querySelectorAll(".revealL");
-})
+    for (let i = 0; i < reveal.length; i++) {
+        if (window.pageYOffset > 500) {
+            reveal[i].classList.add("active");
+        }
+        else{
+            reveal[i].classList.remove("active");
+        }
+};
+});
