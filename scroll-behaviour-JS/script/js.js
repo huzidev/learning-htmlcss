37c137d8696b8decb3d3e036e6ddea18d281bd6c
huzidev <huzaifa.iqdev@gmail.com>
2021-12-21 20:12:40 +0500

    function name

--- scroll-behaviour-JS/script/js.js
+++ scroll-behaviour-JS/script/js.js
@@ -1,7 +1,14 @@
 const skills = document.getElementById("skills-section");
 const progress = document.querySelectorAll(".progress-bar");
 
+function showProgress() {
+    
+}
+
+
 window.addEventListener("scroll", function () {
     const content = skills.getBoundingClientRect().top;
     const screen = window.innerHeight;
+    if (content < screen) {
+    }
 })
