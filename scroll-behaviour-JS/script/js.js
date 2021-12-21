46c8ea05676cf81c565f5f334446c3082cffaadf
huzidev <huzaifa.iqdev@gmail.com>
2021-12-21 20:11:01 +0500

    bounding event

--- scroll-behaviour-JS/script/js.js
+++ scroll-behaviour-JS/script/js.js
@@ -2,5 +2,5 @@ const skills = document.getElementById("skills-section");
 const progress = document.querySelectorAll(".progress-bar");
 
 window.addEventListener("scroll", function () {
-    
+    const content = skills.getBoundingClientRect().top;
 })
