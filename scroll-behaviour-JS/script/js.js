2f4e5ecd4a1330096ed33acff58dda492a4c6fd9
huzidev <huzaifa.iqdev@gmail.com>
2021-12-21 20:11:17 +0500

    widnows height

--- scroll-behaviour-JS/script/js.js
+++ scroll-behaviour-JS/script/js.js
@@ -3,4 +3,5 @@ const progress = document.querySelectorAll(".progress-bar");
 
 window.addEventListener("scroll", function () {
     const content = skills.getBoundingClientRect().top;
+    const screen = window.innerHeight;
 })
