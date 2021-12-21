1605115d402a6f0135cf0abb89a1a7b6425ce248
huzidev <huzaifa.iqdev@gmail.com>
2021-12-21 10:43:11 +0500

    windows height

--- web-scroll-page/script/js.js
+++ web-scroll-page/script/js.js
@@ -1,5 +1,6 @@
 window.addEventListener("scroll", ()=>{
     let revealR = document.querySelector(".reveal.R");
     let content = revealR.getBoundingClientRect().top;
+    let screen = window.innerHeight;
 
 });
