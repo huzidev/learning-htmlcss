b6f0c492a1d722473b8cdd12b71e2bb0d43d1bfd
huzidev <huzaifa.iqdev@gmail.com>
2021-12-21 12:28:18 +0500

    scroll fix

--- web-scroll-page/script/js.js
+++ web-scroll-page/script/js.js
@@ -54,7 +54,22 @@ window.addEventListener("scroll", function () {
 });
 
 window.addEventListener("scroll", function () {
-    let revealL = document.querySelector(".revealR1");
+    let revealL = document.querySelectorAll(".revealR1");
+    for (let i = 0; i < revealL.length; i++) {
+        let content = revealL[i].getBoundingClientRect().top;
+        let screen = window.innerHeight;
+        if (content < screen) {
+            revealL[i].classList.add("active");
+        }
+        else {
+            revealL[i].classList.remove("active");
+        }
+    }
+});
+
+
+window.addEventListener("scroll", function () {
+    let revealL = document.querySelector(".revealR2");
     let content = revealL.getBoundingClientRect().top;
     let screen = window.innerHeight;
     if (content < screen) {
