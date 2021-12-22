01514c63341d1a2bff38e7388026d75b9ef949db
huzidev <huzaifa.iqdev@gmail.com>
2021-12-22 15:55:13 +0500

    position

--- Math-Dimensions/script/test.js
+++ Math-Dimensions/script/test.js
@@ -2,6 +2,7 @@ window.addEventListener("load", ()=>{
     const square = document.querySelector(".ball-1");
     const SQ = {h: square.clientHeight, w: square.clientWidth};
     const result = {y: (window.innerHeight - SQ.h), x:(window.innerWidth - SQ.w)};
-
+    square.style.top = result.y+"px";
+    square.style.right = result.x+"px";
 
 })
