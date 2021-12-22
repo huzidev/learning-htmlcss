c919b80e9d213624be317703011c89a5a82bd88a
huzidev <huzaifa.iqdev@gmail.com>
2021-12-22 15:52:55 +0500

    client height

--- Math-Dimensions/script/test.js
+++ Math-Dimensions/script/test.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", ()=>{
     const square = document.querySelector(".ball-1");
     const SQ = {h: square.clientHeight, w: square.clientWidth};
+    const result = {y: (window.innerHeight - SQ.h), x:(window.innerWidth - SQ.w)};
 
 
 })
