c419e093d3374ac7561d0cb20a891fa234029d6a
huzidev <huzaifa.iqdev@gmail.com>
2021-12-22 13:07:59 +0500

    function fix

--- Pure-Slider-JS/script/js.js
+++ Pure-Slider-JS/script/js.js
@@ -2,5 +2,7 @@ let slide = document.querySelectorAll(".slide-container");
 let i = o;
 
 function next() {
-    slide[i].classList.remove("active")
+    slide[i].classList.remove("active");
+    i = (i +1) % slide.length;
+    slide[i].classList.add("active");
 }
