3c624149a712fcf3b9605494efa40f759f153c3d
huzidev <huzaifa.iqdev@gmail.com>
2021-12-22 11:45:47 +0500

    merge

--- Live-Button-JS/script/js.js
+++ Live-Button-JS/script/js.js
@@ -1,5 +1,5 @@
 var btn = document.getElementById("btn");
-var btn = document.getElementById("light");
+var light = document.getElementById("light");
 function toggleBtn() { // BASICALLY TOGGLEBTN() IS THE FUNCTION OF OURS BUTTON TAG
     btn.classList.toggle("active"); // MEANS WHENEVER WE CLICKS THE BUTTON WHOSE ID IS (BTN) AS WE GAVE TO THE BUTTON ELEMENT THE CLASS LIST WILL BE ADDED NAME "ACTIVE"
     // REMEMBER TO USE TOGGLE NOT ADD BCZ TOGGLE MEANS WHENEVER WE TOGGLE(MEANS = CLICK OR NOT CLICK)
