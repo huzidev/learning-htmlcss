4c2cdfd85ddf2ac0aa09d3b3e119ef0e6c46a191
huzidev <huzaifa.iqdev@gmail.com>
2021-12-23 21:27:52 +0500

    js setting

--- Animated-Drop-Down/script/js.js
+++ Animated-Drop-Down/script/js.js
@@ -1,10 +1,15 @@
 const dropBtn = document.querySelector(".drop-btn");
 const menuWrapper = document.querySelector(".wrapper");
+const menuBar = document.querySelector(".menu-bar");
 const settingDrop = document.querySelector(".setting-drop");
 const helpDrop = document.querySelector(".help-drop");
 const settingItem = document.querySelector(".setting-item");
 const helpItem = document.querySelector(".help-item");
 dropBtn.onclick = (()=>{
     menuWrapper.classList.toggle("active");
+
+})
+settingItem.onclick = (() => {
+    menuBar.classList.toggle("active");
 
 })
