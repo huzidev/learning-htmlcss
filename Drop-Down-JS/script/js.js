4da5685812097c7b9d7a48f31189b120f805b5ce
huzidev <huzaifa.iqdev@gmail.com>
2021-12-23 15:39:07 +0500

    sub-menu

--- Drop-Down-JS/script/js.js
+++ Drop-Down-JS/script/js.js
@@ -1,8 +1,20 @@
 const a_parent = document.querySelectorAll(".a_parent"); // ALL OF OURS ANCHOR TAG IS OF THIS CLASSs
+const a_parent = document.querySelectorAll(".dd_menu_a");
 
 a_parent.forEach(function (a_parent_item) {
     a_parent_item.addEventListener("click", function () {
         
+        a_parent.forEach(function (a_parent_item) {
+            a_parent_item.classList.remove("active");
+        })
+        a_parent_item.classList.add("active");
+    })
+})
+
+
+a_parent.forEach(function (a_parent_item) {
+    a_parent_item.addEventListener("click", function () {
+
         a_parent.forEach(function (a_parent_item) {
             a_parent_item.classList.remove("active");
         })
