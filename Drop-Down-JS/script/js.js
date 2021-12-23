fd4b9fde881cec423824fbcac8aa987f8bb2b844
huzidev <huzaifa.iqdev@gmail.com>
2021-12-23 16:36:57 +0500

    dropdown fix

--- Drop-Down-JS/script/js.js
+++ Drop-Down-JS/script/js.js
@@ -3,13 +3,13 @@ const dd_menu_a = document.querySelectorAll(".dd_menu_a");
 
 a_parent.forEach(function (a_parent_item) {
     a_parent_item.addEventListener("click", function () {
-        dd_menu_a.forEach(function (dd_menu_a_item) {
-            dd_menu_a_item.classList.remove("active");
-        })
-        a_parent.forEach(function (a_parent_item) {
-            a_parent_item.classList.remove("active");
-        })
-        a_parent_item.classList.add("active");
+        // dd_menu_a.forEach(function (dd_menu_a_item) {
+        //     dd_menu_a_item.classList.remove("active");
+        // })
+        // a_parent.forEach(function (a_parent_item) {
+        //     a_parent_item.classList.remove("active");
+        // })
+        a_parent_item.classList.toggle("active");
     })
 })
 
@@ -17,9 +17,9 @@ a_parent.forEach(function (a_parent_item) {
 dd_menu_a.forEach(function (dd_menu_a_item) {
     dd_menu_a_item.addEventListener("click", function () {
 
-        dd_menu_a.forEach(function (dd_menu_a_item) {
-            dd_menu_a_item.classList.remove("active");
-        })
-        dd_menu_a_item.classList.add("active");
+        // dd_menu_a.forEach(function (dd_menu_a_item) {
+        //     dd_menu_a_item.classList.remove("active");
+        // })
+        dd_menu_a_item.classList.toggle("active");
     })
 })
