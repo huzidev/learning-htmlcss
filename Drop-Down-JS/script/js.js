dbbdee8cef2b2698c6022e7a1a4bed5a271b0013
huzidev <huzaifa.iqdev@gmail.com>
2021-12-23 11:18:42 +0500

    toggle for classList

--- Drop-Down-JS/script/js.js
+++ Drop-Down-JS/script/js.js
@@ -1,4 +1,5 @@
 document.getElementById("menu").addEventListener("click", open);
 function open() {
     document.getElementById("dropdown").classList.toggle("active");
+    document.getElementById("dropdown1").classList.toggle("active");
 };
