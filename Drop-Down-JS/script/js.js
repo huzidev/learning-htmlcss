66ac374e15c18eb00134f02ba193cd465a0fe4bb
huzidev <huzaifa.iqdev@gmail.com>
2021-12-23 15:22:58 +0500

    js

--- Drop-Down-JS/script/js.js
+++ Drop-Down-JS/script/js.js
@@ -1,1 +1,11 @@
-const a_parent = document.querySelectorAll(".a_parent")
+const a_parent = document.querySelectorAll(".a_parent"); // ALL OF OURS ANCHOR TAG IS OF THIS CLASSs
+
+a_parent.forEach(function (a_parent_item) {
+    a_parent_item.addEventListener("click", function () {
+        
+        a_parent.forEach(function (a_parent_item) {
+            a_parent_item.classList.remove("active");
+        })
+        a_parent_item.classList.add("active");
+    })
+})
