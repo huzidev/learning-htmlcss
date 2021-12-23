b64b208dff4107c5b4adeb58cb38a8ad98dd034c
huzidev <huzaifa.iqdev@gmail.com>
2021-12-23 21:22:31 +0500

    querey slector JS

--- Animated-Drop-Down/script/js.js
+++ Animated-Drop-Down/script/js.js
@@ -1,5 +1,7 @@
 const dropBtn = document.querySelector(".drop-btn");
 const menuWrapper = document.querySelector(".wrapper");
+const settingDrop = document.querySelector(".setting-drop");
+const menuBar = document.querySelector(".menu-bar");
 dropBtn.onclick = (()=>{
     menuWrapper.classList.toggle("active");
 
