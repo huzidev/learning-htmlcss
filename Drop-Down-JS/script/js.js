772a119470495caf11511d7431f71c6391a4d61b
huzidev <huzaifa.iqdev@gmail.com>
2021-12-23 17:22:26 +0500

    sass

--- Drop-Down-JS/script/js.js
+++ Drop-Down-JS/script/js.js
@@ -3,12 +3,6 @@ const dd_menu_a = document.querySelectorAll(".ToDo");
 
 a_parent.forEach(function (a_parent_item) {
     a_parent_item.addEventListener("click", function () {
-        // dd_menu_a.forEach(function (dd_menu_a_item) {
-        //     dd_menu_a_item.classList.remove("active");
-        // })
-        // a_parent.forEach(function (a_parent_item) {
-        //     a_parent_item.classList.remove("active");
-        // })
         a_parent_item.classList.toggle("active");
     });
 });
@@ -16,10 +10,6 @@ a_parent.forEach(function (a_parent_item) {
 
 dd_menu_a.forEach(function (dd_menu_a_item) {
     dd_menu_a_item.addEventListener("click", function () {
-
-        // dd_menu_a.forEach(function (dd_menu_a_item) {
-        //     dd_menu_a_item.classList.remove("active");
-        // })
         dd_menu_a_item.classList.toggle("active");
     });
 });
