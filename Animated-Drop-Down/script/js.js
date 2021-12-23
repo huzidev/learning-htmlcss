9f8054f2967e0412b631a82efff1b1b5100fc055
huzidev <huzaifa.iqdev@gmail.com>
2021-12-23 21:26:11 +0500

    js occurennce

--- Animated-Drop-Down/script/js.js
+++ Animated-Drop-Down/script/js.js
@@ -2,6 +2,8 @@ const dropBtn = document.querySelector(".drop-btn");
 const menuWrapper = document.querySelector(".wrapper");
 const settingDrop = document.querySelector(".setting-drop");
 const helpDrop = document.querySelector(".help-drop");
+const settingItem = document.querySelector(".setting-item");
+const helpItem = document.querySelector(".help-item");
 dropBtn.onclick = (()=>{
     menuWrapper.classList.toggle("active");
 
