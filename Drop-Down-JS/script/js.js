570203d6393e6832c62d55076431c18ea5b99392
huzidev <huzaifa.iqdev@gmail.com>
2021-12-23 15:42:41 +0500

    JS origin

--- Drop-Down-JS/script/js.js
+++ Drop-Down-JS/script/js.js
@@ -3,7 +3,9 @@ const a_parent = document.querySelectorAll(".dd_menu_a");
 
 a_parent.forEach(function (a_parent_item) {
     a_parent_item.addEventListener("click", function () {
-        
+        dd_menu_a.forEach(function (dd_menu_a_item) {
+            dd_menu_a_item.classList.remove("active");
+        })
         a_parent.forEach(function (a_parent_item) {
             a_parent_item.classList.remove("active");
         })
@@ -12,12 +14,12 @@ a_parent.forEach(function (a_parent_item) {
 })
 
 
-a_parent.forEach(function (a_parent_item) {
-    a_parent_item.addEventListener("click", function () {
+dd_menu_a.forEach(function (dd_menu_a_item) {
+    dd_menu_a_item.addEventListener("click", function () {
 
-        a_parent.forEach(function (a_parent_item) {
-            a_parent_item.classList.remove("active");
+        dd_menu_a.forEach(function (dd_menu_a_item) {
+            dd_menu_a_item.classList.remove("active");
         })
-        a_parent_item.classList.add("active");
+        dd_menu_a_item.classList.add("active");
     })
 })
