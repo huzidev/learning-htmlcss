e0e83b2ae804d93324dc53e27c1b3d33dca00edb
huzidev <huzaifa.iqdev@gmail.com>
2021-12-23 11:20:11 +0500

    element by id

--- Drop-Down-JS/script/js.js
+++ Drop-Down-JS/script/js.js
@@ -1,5 +1,7 @@
 document.getElementById("menu").addEventListener("click", open);
 function open() {
     document.getElementById("dropdown").classList.toggle("active");
-    document.getElementById("dropdown1").classList.toggle("active");
 };
+
+document.getElementById("menu-1").addEventListener("click", openMenu);
+document.getElementById("dropdown1").classList.toggle("active");
