a3b63d72e2f8daad88139374adb3b12bbd44cd1d
huzidev <huzaifa.iqdev@gmail.com>
2021-12-23 10:35:43 +0500

    inentations

--- Drop-Down-JS/script/js.js
+++ Drop-Down-JS/script/js.js
@@ -1,4 +1,4 @@
 document.getElementById("menu").addEventListener("click", open);
 function open() {
-    document.getElementById("dropdown").classList.toggle("active")
-}
+    document.getElementById("dropdown").classList.toggle("active");
+};
