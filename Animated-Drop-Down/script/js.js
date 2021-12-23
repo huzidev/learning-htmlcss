5fa47291ddfd7092890403f6c57eefedeab2f3a1
huzidev <huzaifa.iqdev@gmail.com>
2021-12-23 21:20:46 +0500

    toggl

--- Animated-Drop-Down/script/js.js
+++ Animated-Drop-Down/script/js.js
@@ -1,2 +1,6 @@
 const dropBtn = document.querySelector(".drop-btn");
 const menuWrapper = document.querySelector(".wrapper");
+dropBtn.onclick = (()=>{
+    menuWrapper.classList.toggle("active");
+
+})
