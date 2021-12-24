61274dfaecdc1bf6ba44445ff960a5134a7c427c
huzidev <huzaifa.iqdev@gmail.com>
2021-12-24 16:16:16 +0500

    focus

--- Animated-Login-Page/script/js.js
+++ Animated-Login-Page/script/js.js
@@ -5,7 +5,14 @@ function focus() {
     parent.classList.add("focus");
 };
 
+function blur() { // SO WHEN WE CLICK ANY OTHER THAN INPUT THE FOCUS WILL BE REMOVED
+    let parent = this.parentNode.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus");
+    }
+};
+
 input.forEach(input =>{
     input.addEventListener("focus", focus)
-
+    input.addEventListener("blur", blur)
 })
