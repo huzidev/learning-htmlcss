b6abf91ee32ab086983788106bc5a6545dabe79f
huzidev <huzaifa.iqdev@gmail.com>
2021-12-24 16:17:25 +0500

    comment

--- Animated-Login-Page/script/js.js
+++ Animated-Login-Page/script/js.js
@@ -2,7 +2,7 @@ const input = document.querySelectorAll(".input");
 
 function focus() {
     let parent = this.parentNode.parentNode;
-    parent.classList.add("focus");
+    parent.classList.add("focus"); // FOCUS MEANS THE WRITTEN USERNAME OR PASSWORD WILL MOVE -5PX WHEN WE CLICK ON THE INPUT ELEMENT
 };
 
 function blur() { // SO WHEN WE CLICK ANY OTHER THAN INPUT THE FOCUS WILL BE REMOVED
