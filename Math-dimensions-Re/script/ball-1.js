4bb439046fc4ff025ab0f3f0774b0615912221e9
huzidev <huzaifa.iqdev@gmail.com>
2021-12-25 10:59:23 +0500

    comment

--- Math-dimensions-Re/script/ball-1.js
+++ Math-dimensions-Re/script/ball-1.js
@@ -1,12 +1,32 @@
 window.addEventListener("load", ()=> {
     const round = document.getElementsByClassName("ball-1")[0];
-    const R = {h:  round.clientHeight, w: round.clientWidth };
-    const final = {y:  (window.innerHeight - R.h), x: (window.innerWidth - R.w)};
-    round.style.top = final.y+"px";
-    round.style.right = final.x+"px";
-    window.addEventListener("scroll", ()=>{
-        
+    // const R = {h:  round.clientHeight, w: round.clientWidth };
 
+    
+    const Rheight = round.clientHeight // MEANS HEIGHT OF OURS DIV
+    const Rwidth = round.clientWidth // MEANS WIDTH OF OURS DIV
+
+
+    // const Rheight = 100 // MEANS EXACT HEIGHT OF OURS DIV
+    // const Rwidth = 100 // MEANS EXACT WIDTH OF OURS DIV
+
+
+    const finalY = (window.innerHeight - Rheight)
+    const finalX = (window.innerWidth - Rwidth)
+    round.style.top = finalY+"px";
+    round.style.right = finalX+"px";
+    window.addEventListener("scroll", ()=>{
+        // const height = document.body.clientHeight - window.innerHeight;
+        const width = document.body.clientWidth;
+        // const scroll = window.scrollY
+
+        const ratio = (window.scrollY) / (2000 - window.innerHeight)// BUT NOT IN EVERY CASES WE KNOWS EXACT HEIGHT OF THE BODY CONTENT BUT WE CAN FIND THE EXACT HEIGHT OF BODY CONTENT
+
+        // const ratio = (window.scrollY) / (document.body.clientHeight - window.innerHeight)
+        const dy = finalY - (ratio * window.innerHeight);
+        const dx = finalX - (ratio * window.innerWidth);
+        round.style.top = `${dy}px`
+        round.style.right = `${dx}px`
     })
 
 })
