c53338f02aee4fc8f3a4c043041b14fef1cb668f
huzidev <huzaifa.iqdev@gmail.com>
2021-12-28 10:15:48 +0500

    domenison from center

--- Math-dimensions-Re/script/center.js
+++ Math-dimensions-Re/script/center.js
@@ -10,6 +10,17 @@ window.addEventListener("load", function () {
         const height = this.document.body.clientHeight - window.innerHeight;
         const width = this.document.body.clientWidth;
         const ratio = (window.scrollY) / (height)
-        
+        var dy = RY - (ratio * RY)
+        var dx = RX - (ratio * RX)
+        center.style.bottom = `${dy}px`
+        center.style.right = `${dx}px`
+        console.log(dy, dx, ratio);
+        if (ratio > 0.5) {
+            // const dxx = (RX - (0.5 * RX)) - (RX - (ratio * RX))
+            // dx = (RX * 0.5) + (dxx)
+            r = 1.0 - ratio;
+            dx = RX - (r * RX);
+            center.style.right = `${dx}px`
+        }
     })
 })
