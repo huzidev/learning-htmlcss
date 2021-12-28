5f46ffc72f22a228eb2a214ccc2c20e660908c93
huzidev <huzaifa.iqdev@gmail.com>
2021-12-28 18:54:01 +0500

    maths by JS

--- Math-dimensions-Re/script/top.js
+++ Math-dimensions-Re/script/top.js
@@ -1,12 +1,18 @@
 window.addEventListener("load", function () {
     const top = this.document.getElementsByClassName("ball-6")[0];
-    const TH = corner.clientWidth;
-    const TW = corner.clientHeight;
-    const RY = window.innerHeight - CH;
-    const RX = window.innerWidth - CW;
-    corner.style.bottom = RY+"px"
-    corner.style.right = RX+"px"
+    const TH = top.clientWidth;
+    const TW = top.clientHeight;
+    const RY = window.innerHeight - TH;
+    const RX = window.innerWidth - TW;
+    top.style.bottom = RY+"px"
+    top.style.right = RX+"px"
     window.addEventListener("scroll", function () {
         const height = this.document.body.clientHeight - window.innerHeight;
         const width = this.document.body.clientWidth;
-        const ratio = (window.scrollY) / (height);
+        const ratio = (window.scrollY) / (height)
+        var dy = RY - (ratio * (RY))
+        var dx = RX - (ratio * (RX))
+        top.style.bottom = `${dy}px`
+        top.style.right = `${dx}px`
+    })
+})
