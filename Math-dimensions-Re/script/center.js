a28d8b417025e46a2a84a146b02855dc9c81bab3
huzidev <huzaifa.iqdev@gmail.com>
2021-12-28 10:03:00 +0500

    height & widht

--- Math-dimensions-Re/script/center.js
+++ Math-dimensions-Re/script/center.js
@@ -1,3 +1,13 @@
 window.addEventListener("load", function () {
     const center = this.document.getElementsByClassName("ball-3")[0];
+    const CH = center.clientHeight;
+    const CW = center.clientWidth;
+    const RY = window.innerHeight - CH
+    const RX = window.innerWidth - CW
+    center.style.bottom = RY+"px"
+    center.style.right = RX+"px"
+    window.addEventListener("scroll", function () {
+        const height = this.document.body.clientHeight - window.innerHeight;
+        const width = this.document.body.clientWidth;
+    })
 })
