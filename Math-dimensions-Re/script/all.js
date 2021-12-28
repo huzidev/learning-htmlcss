245bcf7fb6d7cc67600f40a8304c13a1c1a09e0b
huzidev <huzaifa.iqdev@gmail.com>
2021-12-28 21:08:48 +0500

    comment

--- Math-dimensions-Re/script/all.js
+++ Math-dimensions-Re/script/all.js
@@ -4,15 +4,20 @@ window.addEventListener("load", function () {
     const AW = all.clientWidth;
     const RY = window.innerHeight - AH;
     const RX = window.innerWidth - AW;
-    all.style.bottom = RY+"px"
-    all.style.right = RX+"px"
+    all.style.right = RY+"px"
+    all.style.center = RX+"px"
     window.addEventListener("scroll", function () {
         const height = this.document.body.clientHeight - window.innerHeight;
         const ratio = (window.scrollY) / (height)
         var dy = RY - (ratio * (RY))
         var dx = RX - (ratio * (RX))
         all.style.bottom = `${dy}px`
-        all.style.right = `${dx}px`
-        
+        all.style.center = `${dx}px`
+        if (ratio > 0.5) {
+            all.style.right = `${dx}px` // BECAUSE WE WANTED MOVEMENT IN THE HORIZONTAL AXIS
+            // all.style.left = `${dx}px`
+            // all.style.right = `${dy}px`
+
+        }
     })
 })
