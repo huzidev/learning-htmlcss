2712bd1b3366a6f7ced1fee0e09cffceacb68564
huzidev <huzaifa.iqdev@gmail.com>
2021-12-28 17:25:12 +0500

    boiler plater

---
+++ Math-dimensions-Re/script/all.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function () {
+    const all = this.document.getElementsByClassName("ball-6")[0];
+    const AH = all.clientHeight
+    const AW = all.clientWidth;
+    const RY = window.innerHeight - AH
+    const RX = window.innerWidth - AW
+    
+})
