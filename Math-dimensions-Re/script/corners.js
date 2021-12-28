84e5ed15ae91fa3b69918c98916a2af1b5d642f7
huzidev <huzaifa.iqdev@gmail.com>
2021-12-28 11:40:03 +0500

    ratio

--- Math-dimensions-Re/script/corners.js
+++ Math-dimensions-Re/script/corners.js
@@ -1,3 +1,14 @@
 window.addEventListener("load", function () {
     const corner = this.document.getElementsByClassName("ball-4")[0];
+    const CW = corner.clientHeight;
+    const CH = corner.clientWidth;
+    const RY = window.innerHeight - CH;
+    const RX = window.innerWidth - CW;
+    corner.style.bottom = RY+"px"
+    corner.style.right = RX+"px"
+    window.addEventListener("scroll", function () {
+        const height = this.document.body.clientHeight - window.innerHeight;
+        const width = this.document.body.clientWidth;
+        const ratio = (window.scrollY) / (height);
+    })
 })
