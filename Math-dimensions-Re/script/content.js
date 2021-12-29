f28c805a1f1a01a08171c04a487447fc278d3066
huzidev <huzaifa.iqdev@gmail.com>
2021-12-29 11:35:58 +0500

    y-axis math

--- Math-dimensions-Re/script/content.js
+++ Math-dimensions-Re/script/content.js
@@ -11,6 +11,6 @@ window.addEventListener("load", function () {
         const ratio = (window.scrollY) / (height)
         var dy = RY - (ratio * RY);
         var dx = RX - (ratio * RX);
-        content.style.bottom = `${dy / 2}px`
+        content.style.bottom = `${dy / 2}px` // THIS METHOD IS USED IF WE WANTED TO MOVE IT FROM HALF OF THE Y-AXIS MEANS FROM THE CENTER OF THE PAGE
     })
 })
