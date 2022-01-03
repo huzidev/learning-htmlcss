ec053f0dcf29cca1ccbf472480ce8b0d05c39e5c
huzidev <huzaifa.iqdev@gmail.com>
2022-01-03 14:12:05 +0500

    rx

--- Math-dimensions-Re/script/all.js
+++ Math-dimensions-Re/script/all.js
@@ -1,41 +1,41 @@
-window.addEventListener("load", function () {
-    const all = this.document.getElementsByClassName("ball-8")[0];
-    const AH = all.clientHeight;
-    const AW = all.clientWidth;
-    const RY = window.innerHeight - AH;
-    const RX = window.innerWidth - AW;
-    all.style.bottom = RY+"px"
-    all.style.right = (RX / 2)+"px"
-    window.addEventListener("scroll", function () {
-        const height = this.document.body.clientHeight - window.innerHeight;
-        const ratio = (window.scrollY) / (height)
-        var dy = RY - (ratio * (RY))
-        var dx = RX - (ratio * (RX))
-        all.style.bottom = `${dy}px`
-        if (ratio >= 0.5 && ratio <= 1.0) {
-            all.style.right = `${dx}px` // BECAUSE WE WANTED MOVEMENT IN THE HORIZONTAL AXIS
-        }
-    });
-});
+// window.addEventListener("load", function () {
+//     const all = this.document.getElementsByClassName("ball-8")[0];
+//     const AH = all.clientHeight;
+//     const AW = all.clientWidth;
+//     const RY = window.innerHeight - AH;
+//     const RX = window.innerWidth - AW;
+//     all.style.bottom = RY+"px"
+//     all.style.right = (RX / 2)+"px"
+//     window.addEventListener("scroll", function () {
+//         const height = this.document.body.clientHeight - window.innerHeight;
+//         const ratio = (window.scrollY) / (height)
+//         var dy = RY - (ratio * (RY))
+//         var dx = RX - (ratio * (RX))
+//         all.style.bottom = `${dy}px`
+//         if (ratio >= 0.5 && ratio <= 1.0) {
+//             all.style.right = `${dx}px` // BECAUSE WE WANTED MOVEMENT IN THE HORIZONTAL AXIS
+//         }
+//     });
+// });
 
 
-window.addEventListener("load", function () {
-    const all = this.document.getElementsByClassName("ball-9")[0];
-    const AH = all.clientHeight;
-    const AW = all.clientWidth;
-    const RY = window.innerHeight - AH;
-    const RX = window.innerWidth - AW;
-    all.style.bottom = RY + "px"
-    all.style.right = (RX / 2) + "px"
-    window.addEventListener("scroll", function () {
-        const height = this.document.body.clientHeight - window.innerHeight;
-        const ratio = (window.scrollY) / (height)
-        var dy = RY - (ratio * (RY))
-        var dx = RX - (ratio * (RX))
-        all.style.bottom = `${dy}px`
-        if (ratio >= 0.5 && ratio <= 1.0) {
-            // all.style.right = `${dx}px` // BECAUSE WE WANTED MOVEMENT IN THE HORIZONTAL AXIS
-            all.style.left = `${dx}px`
-        }
-    });
-});
+// window.addEventListener("load", function () {
+//     const all = this.document.getElementsByClassName("ball-9")[0];
+//     const AH = all.clientHeight;
+//     const AW = all.clientWidth;
+//     const RY = window.innerHeight - AH;
+//     const RX = window.innerWidth - AW;
+//     all.style.bottom = RY + "px"
+//     all.style.right = (RX / 2) + "px"
+//     window.addEventListener("scroll", function () {
+//         const height = this.document.body.clientHeight - window.innerHeight;
+//         const ratio = (window.scrollY) / (height)
+//         var dy = RY - (ratio * (RY))
+//         var dx = RX - (ratio * (RX))
+//         all.style.bottom = `${dy}px`
+//         if (ratio >= 0.5 && ratio <= 1.0) {
+//             // all.style.right = `${dx}px` // BECAUSE WE WANTED MOVEMENT IN THE HORIZONTAL AXIS
+//             all.style.left = `${dx}px`
+//         }
+//     });
+// });
