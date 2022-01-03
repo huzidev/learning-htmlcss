3e5358dc66e8b0d510da7b88007ce15bc83bb8a8
huzidev <huzaifa.iqdev@gmail.com>
2022-01-03 11:53:12 +0500

    js occurence

--- Math-dimensions-Re/script/content.js
+++ Math-dimensions-Re/script/content.js
@@ -9,13 +9,13 @@ window.addEventListener("load", function () {
     window.addEventListener("scroll", function () {
         const height = this.document.body.clientHeight - window.innerHeight
         const ratio = (window.scrollY) / (height)
-        var dy = RY - (ratio * (RY * 2));
+        var dy = RY - (ratio * (RY * 2)); 
         var dx = RX - (ratio * (RX ) * 2);
         content.style.bottom = `${dy / 2}px` // THIS METHOD IS USED IF WE WANTED TO MOVE IT FROM HALF OF THE Y-AXIS MEANS FROM THE CENTER OF THE PAGE
         content.style.right = `${dx}px`
-        if (ratio > 0.5) {
-            dx = RX - RX
-            content.style.right = `${dx}px`
+        if (ratio >= 0.5 && ratio <=1.0 ) {
+            // dx = RX - RX
+            content.style.right = `0px` // MEANS NO MOVEMENT AT RIGHT SIDE OF THE PAGE 
             content.style.bottom = `${-dy}px` // IF WE USED (DY / 2) SO IT'LL JUST COVER HALF OF THE DISTANCE OF THE PAGE
         }
     })
