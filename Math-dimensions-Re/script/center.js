86a21ac437e55000d4a5cd0c89a934c4114c478e
huzidev <huzaifa.iqdev@gmail.com>
2022-01-03 16:16:39 +0500

    star

--- Math-dimensions-Re/script/center.js
+++ Math-dimensions-Re/script/center.js
@@ -1,27 +1,27 @@
-window.addEventListener("load", function () {
-    const center = this.document.getElementsByClassName("ball-3")[0];
-    const CH = center.clientHeight;
-    const CW = center.clientWidth;
-    const RY = window.innerHeight - CH
-    const RX = window.innerWidth - CW
-    center.style.bottom = RY+"px"
-    center.style.right = RX+"px"
-    window.addEventListener("scroll", function () {
-        const height = this.document.body.clientHeight - window.innerHeight;
-        const width = this.document.body.clientWidth;
-        const ratio = (window.scrollY) / (height)
-        if (ratio >= 0 && ratio <= 0.5) {
-            var dy = RY - ((ratio) * RY)
-            var dx = RX - ((ratio) * RX) // WE MADE IT AN VARIABLE SO IT CAN BE CHANGED AT ANY TIME IF IT WAS AN CONSTANT SO IT CAN'T BE CHANGED AT ANY INSTANCE
-            center.style.bottom = `${dy}px`
-            center.style.right = `${dx}px`
-        }
-        if (ratio > 0.5 && ratio <= 1.0) {
-            r = 1.0 - ratio
-            dy = RY - (ratio * RY )
-            center.style.bottom = `${dy}px` 
-            dx = RX - (r * RX)
-            center.style.right = `${dx}px`
-        }
-    })
-})
+// window.addEventListener("load", function () {
+//     const center = this.document.getElementsByClassName("ball-3")[0];
+//     const CH = center.clientHeight;
+//     const CW = center.clientWidth;
+//     const RY = window.innerHeight - CH
+//     const RX = window.innerWidth - CW
+//     center.style.bottom = RY+"px"
+//     center.style.right = RX+"px"
+//     window.addEventListener("scroll", function () {
+//         const height = this.document.body.clientHeight - window.innerHeight;
+//         const width = this.document.body.clientWidth;
+//         const ratio = (window.scrollY) / (height)
+//         if (ratio >= 0 && ratio <= 0.5) {
+//             var dy = RY - ((ratio) * RY)
+//             var dx = RX - ((ratio) * RX) // WE MADE IT AN VARIABLE SO IT CAN BE CHANGED AT ANY TIME IF IT WAS AN CONSTANT SO IT CAN'T BE CHANGED AT ANY INSTANCE
+//             center.style.bottom = `${dy}px`
+//             center.style.right = `${dx}px`
+//         }
+//         if (ratio > 0.5 && ratio <= 1.0) {
+//             r = 1.0 - ratio
+//             dy = RY - (ratio * RY )
+//             center.style.bottom = `${dy}px` 
+//             dx = RX - (r * RX)
+//             center.style.right = `${dx}px`
+//         }
+//     })
+// })
--- Math-dimensions-Re/script/js.js
+++ Math-dimensions-Re/script/js.js
@@ -1,21 +1,21 @@
-// window.addEventListener("load", function () {
-//     const ball = document.getElementsByClassName("ball")[0];
-//     const B = {h: ball.clientHeight, w: ball.clientWidth}; // AS WE KNOW THAT WHEN WE WRITE DOCUMENT.(BODY.CLIENTHEIGHT) MEANS IT'LL COUNT (BODIES) COMPLETE HEIGHT MEAN ALL THE CONTENT AVAILABLE IN THE (BODY) NOW WE ARE USING (BALL.CLIENTHEIGHT) MEAN BALL HEIGHT WHICH IS 100PX 
-//     const init = {y: (window.innerHeight - B.h), x: (window.innerWidth - B.w)};
-//     ball.style.top = init.y+"px";
-//     ball.style.left = init.x+"px";
-//     window.addEventListener("scroll", function () {
-//         const height = document.body.clientHeight - window.innerHeight;
-//         const width = document.body.clientWidth;
-//         const scroll = window.scrollY;
-//         const ratio = scroll / height;
-//         const dy = init.y - (ratio * (window.innerHeight - B.h));
-//         const dx = init.x - (ratio * (window.innerWidth - B.w));
-//         ball.style.top = `${dy}px`
-//         ball.style.left = `${dx}px`
-//         console.log(B, init, height, width, scroll, ratio, dy, dx);
-//     })
-// });
+window.addEventListener("load", function () {
+    const ball = document.getElementsByClassName("ball")[0];
+    const B = {h: ball.clientHeight, w: ball.clientWidth}; // AS WE KNOW THAT WHEN WE WRITE DOCUMENT.(BODY.CLIENTHEIGHT) MEANS IT'LL COUNT (BODIES) COMPLETE HEIGHT MEAN ALL THE CONTENT AVAILABLE IN THE (BODY) NOW WE ARE USING (BALL.CLIENTHEIGHT) MEAN BALL HEIGHT WHICH IS 100PX 
+    const init = {y: (window.innerHeight - B.h), x: (window.innerWidth - B.w)};
+    ball.style.top = init.y+"px";
+    ball.style.left = init.x+"px";
+    window.addEventListener("scroll", function () {
+        const height = document.body.clientHeight - window.innerHeight;
+        const width = document.body.clientWidth;
+        const scroll = window.scrollY;
+        const ratio = scroll / height;
+        const dy = init.y - (ratio * (window.innerHeight - B.h));
+        const dx = init.x - (ratio * (window.innerWidth - B.w));
+        ball.style.top = `${dy}px`
+        ball.style.left = `${dx}px`
+        console.log(B, init, height, width, scroll, ratio, dy, dx);
+    })
+});
 
 
     
