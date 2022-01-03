b4d6e2e267fa24f0a3f85cf43d0ee7d8f38d0c00
huzidev <huzaifa.iqdev@gmail.com>
2022-01-03 18:38:30 +0500

    fix

--- Math-dimensions-Re/script/star.js
+++ Math-dimensions-Re/script/star.js
@@ -10,8 +10,55 @@ window.addEventListener("load", function () {
         const height = this.document.body.clientHeight - window.innerHeight;
         const ratio = (window.scrollY) / (height)
         if (ratio >= 0 && ratio <= 0.25) {
-            var dy = RY - (ratio * RY);
-            var dx = RX - (ratio * RX);
+            var dy = RY - ((ratio ) * (RY) * 2);
+            var dx = RX - ((ratio ) * (RX) * 4);
+            star.style.bottom = `${dy}px`
+            star.style.right = `${dx / 2}px`
         }
+        else if (ratio > 0.25 && ratio <= 0.50) {
+            dy = RY - (ratio * (RY * 2))
+            dx = RX - (ratio * (RX) * 4)
+            star.style.bottom = `${dy}px`
+            star.style.right = `${-dx / 2}px`
+        }
+        else if (ratio > 0.50 && ratio <= 0.75) {
+            dy = RY - (ratio * (RY * 2))
+            dx = RX - (ratio * (RX) * 4)    
+            star.style.bottom = `${-dy}px`
+            star.style.right = `${-dx / 2}px`
+        }
+        else if (ratio > 0.75 && ratio <= 1.0) {
+            dy = RY - (ratio * (RY) * 2)
+            dx = RX - ((ratio - 0.75) * (RX) * 2 ) // WE USED (-0.75) BECAUSE AS JUST OUTS RATIO HIT (0.75) THE BALL MOVES (0.75) IN RATIO SO WE JUST MINUS THAT VALUE SO IT WON'T JUMP THAT MUCH
+            star.style.bottom = `${-dy}px`
+            star.style.right = `${dx}px`
+
+            // xr = 0.75 + ratio;
+            // r = 1.0 - ratio
+            // dy = RY - (r * (RY) * 2)
+            // // dx = (RX) - (xr * RX)
+            // dx = (RX) - (r  * (RX / 2))
+            // console.log(r, dx, ratio, RX);
+            // star.style.bottom = `${dy}px`
+            // star.style.right = `${dx}px`
+        }
+
+
+        // if (ratio >= 0 && ratio <= 0.25) {
+        //     var dx = RX - ((ratio) * 4 * (RX))
+        //     var dy = RY - (((ratio) * 4) * RY)
+        //     star.style.right = `${dx / 2}px`
+        //     star.style.bottom = `${dy / 2}px`
+        // } 
+        
+        // else if (ratio > 0.25 && ratio <= 0.50) { 
+          
+        // }
+        // else if (ratio > 0.50 && ratio <= 0.75) {
+           
+        // }
+        // else if (ratio > 0.75 && ratio <= 1.0) {
+        
+        // }
     })
 })
