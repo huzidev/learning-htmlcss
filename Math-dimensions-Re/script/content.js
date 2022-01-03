828189bcb524a33006d0d52af9604215e2143294
huzidev <huzaifa.iqdev@gmail.com>
2022-01-03 11:48:40 +0500

    maths

--- Math-dimensions-Re/script/content.js
+++ Math-dimensions-Re/script/content.js
@@ -1,22 +1,22 @@
-// window.addEventListener("load", function () {
-//     const content = this.document.getElementsByClassName("ball-12")[0];
-//     const CH = content.clientHeight;
-//     const CW = content.clientWidth;
-//     const RY = window.innerHeight - CH
-//     const RX = window.innerWidth - CW
-//     content.style.bottom = (window.innerHeight / 2)+"px"
-//     content.style.right = RX+"px"
-//     window.addEventListener("scroll", function () {
-//         const height = this.document.body.clientHeight - window.innerHeight
-//         const ratio = (window.scrollY) / (height)
-//         var dy = RY - (ratio * (RY * 2));
-//         var dx = RX - (ratio * (RX ) * 2);
-//         content.style.bottom = `${dy / 2}px` // THIS METHOD IS USED IF WE WANTED TO MOVE IT FROM HALF OF THE Y-AXIS MEANS FROM THE CENTER OF THE PAGE
-//         content.style.right = `${dx}px`
-//         if (ratio > 0.5) {
-//             dx = RX - RX
-//             content.style.right = `${dx}px`
-//             content.style.bottom = `${- dy / 2}px`
-//         }
-//     })
-// })
+window.addEventListener("load", function () {
+    const content = this.document.getElementsByClassName("ball-12")[0];
+    const CH = content.clientHeight;
+    const CW = content.clientWidth;
+    const RY = window.innerHeight - CH
+    const RX = window.innerWidth - CW
+    content.style.bottom = (window.innerHeight / 2)+"px"
+    content.style.right = RX+"px"
+    window.addEventListener("scroll", function () {
+        const height = this.document.body.clientHeight - window.innerHeight
+        const ratio = (window.scrollY) / (height)
+        var dy = RY - (ratio * (RY * 2));
+        var dx = RX - (ratio * (RX ) * 2);
+        content.style.bottom = `${dy / 2}px` // THIS METHOD IS USED IF WE WANTED TO MOVE IT FROM HALF OF THE Y-AXIS MEANS FROM THE CENTER OF THE PAGE
+        content.style.right = `${dx}px`
+        if (ratio > 0.5) {
+            dx = RX - RX
+            content.style.right = `${dx}px`
+            content.style.bottom = `${-dy}px` // IF WE USED (DY / 2) SO IT'LL JUST COVER HALF OF THE DISTANCE OF THE PAGE
+        }
+    })
+})
--- Math-dimensions-Re/script/corners.js
+++ Math-dimensions-Re/script/corners.js
@@ -1,41 +1,41 @@
-window.addEventListener("load", function () {
-    const corner = this.document.getElementsByClassName("ball-4")[0];
-    const CH = corner.clientHeight;
-    const CW = corner.clientWidth;
-    const RY = window.innerHeight - CH;
-    const RX = window.innerWidth - CW;
-    corner.style.bottom = RY+"px"
-    corner.style.right = RX+"px"
-    window.addEventListener("scroll", function () {
-        const height = this.document.body.clientHeight - window.innerHeight;
-        const width = this.document.body.clientWidth;
-        const ratio = (window.scrollY) / (height);
-        // corner.style.bottom = `${dy}px`
-        // corner.style.right = `${dx}px`  
-        // console.log(ratio);
-        if (ratio >= 0 && ratio <= 0.25) {
-            // var dy = RY - (ratio * RY)
-            var dx = RX - ((ratio * 4) * RX) // MULTIPLY BY FOUR BECAUSE OF FOUR CORNERS
-            corner.style.bottom = `${RY}px` // THIS IS IMPORTANT OTHER WISE THE BALL WILL SKIP FROM TOP IF WE CAME BACK AT HIGHER SPEED AND WE DIDN'T USED (DY) HERE OTHERWISE THE BALL WILL MOVES AT Y-AXIS AS WELL
-            corner.style.right = `${dx}px`
-            // console.log('dx',dx);
-        } else if (ratio > 0.25 && ratio <= 0.50) { // WE DIDN'T USED >= 0.25 OTHERWISE CONFLICT WILL OCCUR BECAUSE WE'VE ALREADY USED <= TO 0.25 ABOVE
-            var dy = RY - (((ratio - 0.25) * 4) * RY); // SO IN EVERY CORNER WE'LL HAVE RATIO EQUAL TO ONE
-            // var dx = RX - ((ratio * 4) * RX)
-            corner.style.bottom = `${dy}px`
-            corner.style.right = `0px`
-        }
-        else if (ratio > 0.50 && ratio <= 0.75){
-            dx = RX - (((ratio - 0.25) * 4) * RX)
-            corner.style.bottom = `0px`
-            corner.style.right = `${-dx}px` // MINUS BECAUSE WE WANT MOVEMENT IN OPPOSITE DIRECTION
-        }
-        else if (ratio > 0.75 && ratio <= 1.0) {
-            r = 1.0 - ratio 
-            // dy = RY - (((ratio - 0.25) * 4) * RY)
-            dy = RY - (r * (RY * 4))
-            corner.style.bottom = `${dy}px`
-            corner.style.right = `${RX}px` // WE DIDN'T USED 0PX THERE BECAUSE IF WE DID SO OURS BALL WILL SHIFT TOWARDS THE RIGHT CORNER WHENEVER THE RATIO BECAME MORE THAN 0.75
-        }
-    });
-});
+// window.addEventListener("load", function () {
+//     const corner = this.document.getElementsByClassName("ball-4")[0];
+//     const CH = corner.clientHeight;
+//     const CW = corner.clientWidth;
+//     const RY = window.innerHeight - CH;
+//     const RX = window.innerWidth - CW;
+//     corner.style.bottom = RY+"px"
+//     corner.style.right = RX+"px"
+//     window.addEventListener("scroll", function () {
+//         const height = this.document.body.clientHeight - window.innerHeight;
+//         const width = this.document.body.clientWidth;
+//         const ratio = (window.scrollY) / (height);
+//         // corner.style.bottom = `${dy}px`
+//         // corner.style.right = `${dx}px`  
+//         // console.log(ratio);
+//         if (ratio >= 0 && ratio <= 0.25) {
+//             // var dy = RY - (ratio * RY)
+//             var dx = RX - ((ratio * 4) * RX) // MULTIPLY BY FOUR BECAUSE OF FOUR CORNERS
+//             corner.style.bottom = `${RY}px` // THIS IS IMPORTANT OTHER WISE THE BALL WILL SKIP FROM TOP IF WE CAME BACK AT HIGHER SPEED AND WE DIDN'T USED (DY) HERE OTHERWISE THE BALL WILL MOVES AT Y-AXIS AS WELL
+//             corner.style.right = `${dx}px`
+//             // console.log('dx',dx);
+//         } else if (ratio > 0.25 && ratio <= 0.50) { // WE DIDN'T USED >= 0.25 OTHERWISE CONFLICT WILL OCCUR BECAUSE WE'VE ALREADY USED <= TO 0.25 ABOVE
+//             var dy = RY - (((ratio - 0.25) * 4) * RY); // SO IN EVERY CORNER WE'LL HAVE RATIO EQUAL TO ONE
+//             // var dx = RX - ((ratio * 4) * RX)
+//             corner.style.bottom = `${dy}px`
+//             corner.style.right = `0px`
+//         }
+//         else if (ratio > 0.50 && ratio <= 0.75){
+//             dx = RX - (((ratio - 0.25) * 4) * RX)
+//             corner.style.bottom = `0px`
+//             corner.style.right = `${-dx}px` // MINUS BECAUSE WE WANT MOVEMENT IN OPPOSITE DIRECTION
+//         }
+//         else if (ratio > 0.75 && ratio <= 1.0) {
+//             r = 1.0 - ratio 
+//             // dy = RY - (((ratio - 0.25) * 4) * RY)
+//             dy = RY - (r * (RY * 4))
+//             corner.style.bottom = `${dy}px`
+//             corner.style.right = `${RX}px` // WE DIDN'T USED 0PX THERE BECAUSE IF WE DID SO OURS BALL WILL SHIFT TOWARDS THE RIGHT CORNER WHENEVER THE RATIO BECAME MORE THAN 0.75
+//         }
+//     });
+// });
