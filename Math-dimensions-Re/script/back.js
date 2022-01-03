a63cd3a8d65574eea549f310fd871bc353777ab9
huzidev <huzaifa.iqdev@gmail.com>
2022-01-03 12:35:23 +0500

    maths

--- Math-dimensions-Re/script/back.js
+++ Math-dimensions-Re/script/back.js
@@ -36,19 +36,14 @@ window.addEventListener("load", function () {
     window.addEventListener("scroll", function () {
         const height = this.document.body.clientHeight - window.innerHeight;
         const ratio = (window.scrollY) / (height)
-        var dy = RY - (ratio * (RY))
-        var dx = RX - (ratio * (RX))
+        var dy = RY - (ratio * RY)
+        var dx = RX - (ratio * RX)
         all.style.bottom = `${dy}px`
-        all.style.center = `${dx}px`
-        if (ratio > 0.5) {
-            all.style.right = `${dx}px` // BECAUSE WE WANTED MOVEMENT IN THE HORIZONTAL AXIS
-            all.style.left = `${dx}px`
-
-            // r = 1.0 - ratio
-            // dy = RY - (r * RY) // SO THE BALL WILL STARTS FROM CENTER AND AS THE RATIO BECAME 0.5 THE BALL WIL MOVE BACK TOWARD THE TOP WITH CHANGING IN HORIZONTAL WIDTH
-            // dx = RX - (r * RX)
-            // all.style.bottom = `${dy}px`
-            // all.style.left = `${dx}px`
+        if (ratio >= 0.5 && ratio <= 1.0) {
+            all.style.bottom = `${dy}px`
+            all.style.left = `${dx}px` // BECAUSE WE WANTED MOVEMENT IN THE HORIZONTAL AXIS
+        
+            
         }
     })
 })
