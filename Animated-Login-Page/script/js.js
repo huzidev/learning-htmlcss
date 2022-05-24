87df6fe85090d3faae6c6b2a83258e010279872d
huzidev <huzaifa.iqdev@gmail.com>
2022-05-24 17:05:53 +0500

    Animated login page completed

--- Animated-Login-Page/script/js.js
+++ Animated-Login-Page/script/js.js
@@ -1,18 +1,20 @@
-const input = document.querySelectorAll(".input");
+const inputs = document.querySelectorAll(".input");
 
-function focus() {
-    let parent = this.parentNode.parentNode;
-    parent.classList.add("focus"); // FOCUS MEANS THE WRITTEN USERNAME OR PASSWORD WILL MOVE -5PX WHEN WE CLICK ON THE INPUT ELEMENT
-};
 
-function blur() { // SO WHEN WE CLICK ANY OTHER THAN INPUT THE FOCUS WILL BE REMOVED
-    let parent = this.parentNode.parentNode;
-    if (this.value == "") {
-        parent.classList.remove("focus");
-    }
-};
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
 
-input.forEach(input =>{
-    input.addEventListener("focus", focus)
-    input.addEventListener("blur", blur)
-})
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
