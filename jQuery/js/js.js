24e38e36dad06d71d2b37aac22607563ed26d642
huzidev <huzaifa.iqdev@gmail.com>
2022-09-12 20:03:34 +0500

    jquerey selectors

--- jQuery/js/js.js
+++ jQuery/js/js.js
@@ -1,4 +1,9 @@
 // when HTML DOM is completely ready
 $(document).ready(function () {
-    
+    $('.p-class').click(function () {
+        window.alert("You've clicked on para which have class")
+    })
+    $('#p-id').click(function () {
+        window.alert("You've clicked on para which have id")
+    })
 })
