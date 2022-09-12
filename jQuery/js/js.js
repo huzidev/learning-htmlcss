890d76f8e9b521547d2d54885476158d9f547b11
huzidev <huzaifa.iqdev@gmail.com>
2022-09-12 20:05:22 +0500

    selectors completed

--- jQuery/js/js.js
+++ jQuery/js/js.js
@@ -1,5 +1,8 @@
 // when HTML DOM is completely ready
 $(document).ready(function () {
+    $('h1').click(function () {
+        window.alert("You've clicked on heading element")
+    })
     $('.p-class').click(function () {
         window.alert("You've clicked on para which have class")
     })
